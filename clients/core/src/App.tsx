aa4d51e60f69dab8f315fd4b1c36aabfb39fd8ba
Stefan Niclas Heun <niclas.heun@tum.de>
2024-11-21 20:30:24 +0100

    adding tanstack the clients

--- clients/core/src/App.tsx
+++ clients/core/src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import ErrorBoundary from './ErrorBoundary'
 import { LandingPage } from './LandingPage/LandingPage'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const TemplateComponent = React.lazy(() => import('template_component/App'))
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 export const App = (): JSX.Element => {
   return (
-    <div>
-      {/* add router here */}
-      <LandingPage />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        {/* add router here */}
+        <LandingPage />
+      </div>
+    </QueryClientProvider>
   )
 }
 
