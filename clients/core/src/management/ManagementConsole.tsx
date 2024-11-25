f3aef3a9d280293276dee7b58daafd832d8285d6
Stefan Niclas Heun <niclas.heun@tum.de>
2024-11-25 18:37:12 +0100

    added unauthorized page

--- clients/core/src/management/ManagementConsole.tsx
+++ clients/core/src/management/ManagementConsole.tsx
@@ -1,26 +1,30 @@
 import { useKeycloak } from '@/keycloak/useKeycloak'
 import { useAuthStore } from '@/zustand/useAuthStore'
+import { Loader2 } from 'lucide-react'
 import { useEffect, useState } from 'react'
+import UnauthorizedPage from './components/UnauthorizedPage'
 
 export const ManagementRoot = (): JSX.Element => {
   const { keycloak, logout } = useKeycloak()
-  const { user } = useAuthStore()
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    if (keycloak && user) {
-      setIsLoading(false)
-    }
-  }, [keycloak, user])
+  const { user, permissions } = useAuthStore()
+  const isLoading = !(keycloak && user)
 
   if (isLoading) {
-    return <div>Loading...</div>
+    return <Loader2 className='h-8 w-8 animate-spin text-primary bg-center' />
+  }
+
+  // TODO update with what was passed to this page
+  if (permissions.length === 0) {
+    return <UnauthorizedPage />
   }
 
   return (
     <div>
       <h1>Management Console</h1>
       <p>Welcome, {user?.firstName}!</p>
+      {permissions.map((permission) => (
+        <p key={permission}>{permission}</p>
+      ))}
       <button onClick={logout}>Logout</button>
     </div>
   )
