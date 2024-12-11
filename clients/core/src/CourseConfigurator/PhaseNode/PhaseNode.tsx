723158fd8cf3a51656a4b908dd68aa854a7b9e27
Stefan Niclas Heun <niclas.heun@tum.de>
2024-12-11 14:24:45 +0100

    increasing the size of the handlers

--- clients/core/src/CourseConfigurator/PhaseNode/PhaseNode.tsx
+++ clients/core/src/CourseConfigurator/PhaseNode/PhaseNode.tsx
@@ -57,60 +57,61 @@ export function PhaseNode({ id, selected }: { id: string; selected?: boolean }) 
   }, [id, setNodes])
 
   return (
-    <Card
-      className={`w-80 shadow-lg hover:shadow-xl transition-shadow duration-300 ${
-        selected ? 'ring-2 ring-blue-500' : ''
-      }`}
-      onClick={onNodeClick}
-    >
-      <CardHeader className='p-4 flex flex-row items-center justify-between bg-gradient-to-r from-blue-50 to-indigo-50'>
-        <div className='flex flex-col'>
-          {isEditing ? (
-            <Input
-              value={phaseData?.name}
-              onChange={(e) => handleNameChange(e.target.value)}
-              className='w-48 mb-2'
-              autoFocus
-              onKeyPress={(e) => e.key === 'Enter' && setIsEditing((prev) => !prev)}
+    <>
+      <Card
+        className={`w-80 shadow-lg hover:shadow-xl transition-shadow duration-300 ${
+          selected ? 'ring-2 ring-blue-500' : ''
+        }`}
+        onClick={onNodeClick}
+      >
+        <CardHeader className='p-4 flex flex-row items-center justify-between bg-gradient-to-r from-blue-50 to-indigo-50'>
+          <div className='flex flex-col'>
+            {isEditing ? (
+              <Input
+                value={phaseData?.name}
+                onChange={(e) => handleNameChange(e.target.value)}
+                className='w-48 mb-2'
+                autoFocus
+                onKeyPress={(e) => e.key === 'Enter' && setIsEditing((prev) => !prev)}
+              />
+            ) : (
+              <CardTitle className='text-lg font-bold text-gray-800 mb-2'>
+                {phaseData?.name}
+              </CardTitle>
+            )}
+            <Badge className='self-start bg-indigo-100 text-indigo-800'>{phaseType?.name}</Badge>
+          </div>
+          <Button variant='ghost' size='icon' onClick={() => setIsEditing((prev) => !prev)}>
+            {isEditing ? <Save className='h-4 w-4' /> : <Pen className='h-4 w-4' />}
+          </Button>
+          {/** TODO DELETE Button */}
+        </CardHeader>
+        <CardContent className='p-4 pt-2'>
+          {phaseType?.input_meta_data && phaseType.input_meta_data.length > 0 && (
+            <MetaDataBadges
+              metaData={phaseType.input_meta_data}
+              icon={<ArrowDownToLine className='w-4 h-4 text-green-500' />}
+              label='Required Input Metadata'
+              providedMetaData={incomingMetaData}
             />
-          ) : (
-            <CardTitle className='text-lg font-bold text-gray-800 mb-2'>
-              {phaseData?.name}
-            </CardTitle>
           )}
-          <Badge className='self-start bg-indigo-100 text-indigo-800'>{phaseType?.name}</Badge>
-        </div>
-        <Button variant='ghost' size='icon' onClick={() => setIsEditing((prev) => !prev)}>
-          {isEditing ? <Save className='h-4 w-4' /> : <Pen className='h-4 w-4' />}
-        </Button>
-        {/** TODO DELETE Button */}
-      </CardHeader>
-      <CardContent className='p-4 pt-2'>
-        {phaseType?.input_meta_data && phaseType.input_meta_data.length > 0 && (
-          <MetaDataBadges
-            metaData={phaseType.input_meta_data}
-            icon={<ArrowDownToLine className='w-4 h-4 text-green-500' />}
-            label='Required Input Metadata'
-            providedMetaData={incomingMetaData}
-          />
-        )}
 
-        {phaseType?.output_meta_data && phaseType.output_meta_data.length > 0 && (
-          <MetaDataBadges
-            metaData={phaseType.output_meta_data}
-            icon={<ArrowUpFromLine className='w-4 h-4 text-green-500' />}
-            label='Provided Output Metadata'
-          />
-        )}
-      </CardContent>
-
+          {phaseType?.output_meta_data && phaseType.output_meta_data.length > 0 && (
+            <MetaDataBadges
+              metaData={phaseType.output_meta_data}
+              icon={<ArrowUpFromLine className='w-4 h-4 text-green-500' />}
+              label='Provided Output Metadata'
+            />
+          )}
+        </CardContent>
+      </Card>
       {!phaseData?.is_initial_phase && (
         <Handle
           type='target'
           position={Position.Top}
           id={`participants-in-${id}`}
-          style={{ left: 50, top: -5 }}
-          className='w-10 h-3 !bg-blue-500 rounded'
+          style={{ left: 50, top: -10 }}
+          className='!w-3 !h-3 !bg-blue-500 rounded-full'
         />
       )}
       {phaseType?.input_meta_data && phaseType.input_meta_data.length > 0 && (
@@ -118,26 +119,26 @@ export function PhaseNode({ id, selected }: { id: string; selected?: boolean }) 
           type='target'
           position={Position.Top}
           id={`metadata-in-${id}`}
-          style={{ left: 130, top: -5 }}
-          className='w-3 h-3 !bg-green-500 rounded-full'
+          style={{ left: 130, top: -10 }}
+          className='!w-3 !h-3 !bg-green-500 rounded-full'
         />
       )}
       <Handle
         type='source'
         position={Position.Bottom}
         id={`participants-out-${id}`}
-        style={{ left: 50, bottom: -5 }}
-        className='w-10 h-3 !bg-blue-500 rounded'
+        style={{ left: 50, bottom: -10 }}
+        className='!w-3 !h-3 !bg-blue-500 rounded-full'
       />
       {phaseType?.output_meta_data && phaseType.output_meta_data.length > 0 && (
         <Handle
           type='source'
           position={Position.Bottom}
           id={`metadata-out-${id}`}
-          style={{ left: 130, bottom: -5 }}
-          className='w-3 h-3 !bg-green-500 rounded-full'
+          style={{ left: 130, bottom: -10 }}
+          className='!w-3 !h-3 !bg-green-500 rounded-full'
         />
       )}
-    </Card>
+    </>
   )
 }
