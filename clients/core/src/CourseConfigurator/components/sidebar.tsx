22e2cfdcda364dfb6e4518aa01dd51a5a3048fb9
Stefan Niclas Heun <niclas.heun@tum.de>
2024-12-11 09:10:55 +0100

    restricting to only one initial phase

--- clients/core/src/CourseConfigurator/components/sidebar.tsx
+++ clients/core/src/CourseConfigurator/components/sidebar.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@/components/ui/button'
-import { phaseTypes } from '../data' // TODO replace this with a DB request
+import { coursePhases, phaseTypes } from '../data' // TODO replace this with a DB request
 
 export function Sidebar() {
   // TODO get this from the database
+  // TODO maybe replace later if multiple are allowed!
+  const courseHasInitialPhase = coursePhases.map((phase) => phase.is_initial_phase).includes(true)
 
   const onDragStart = (event: React.DragEvent<HTMLButtonElement>, nodeType: string) => {
     event.dataTransfer.setData('application/@xyflow/react', nodeType)
@@ -13,17 +15,19 @@ export function Sidebar() {
     <aside className='w-64 bg-gray-100 p-4'>
       <h2 className='text-lg font-semibold mb-4'>Course Phases</h2>
       <div className='space-y-2'>
-        {phaseTypes.map((phase) => (
-          <Button
-            key={phase.id}
-            variant='outline'
-            className='w-full justify-start'
-            draggable
-            onDragStart={(event) => onDragStart(event, phase.id)}
-          >
-            {phase.name}
-          </Button>
-        ))}
+        {phaseTypes.map((phase) =>
+          courseHasInitialPhase && phase.initial_phase ? null : (
+            <Button
+              key={phase.id}
+              variant='outline'
+              className='w-full justify-start'
+              draggable
+              onDragStart={(event) => onDragStart(event, phase.id)}
+            >
+              {phase.name}
+            </Button>
+          ),
+        )}
       </div>
     </aside>
   )
