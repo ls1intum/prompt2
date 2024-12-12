8645bc6a97c40a2688a6b5631d7986df485e87db
Stefan Niclas Heun <niclas.heun@tum.de>
2024-12-12 10:24:32 +0100

    cleaning up server stuff

--- server/coursePhaseType/coursePhaseTypeDTO/course_phase_type.go
+++ server/coursePhaseType/coursePhaseTypeDTO/course_phase_type.go
@@ -3,24 +3,23 @@ package coursePhaseTypeDTO
 import (
 	"github.com/google/uuid"
 	db "github.com/niclasheun/prompt2.0/db/sqlc"
-	"github.com/niclasheun/prompt2.0/meta"
 )
 
 type CoursePhaseType struct {
-	ID                     uuid.UUID     `json:"id"`
-	Name                   string        `json:"name"`
-	RequiredInputMetaData  meta.MetaData `json:"required_input_meta_data"`
-	ProvidedOutputMetaData meta.MetaData `json:"provided_output_meta_data"`
-	InitialPhase           bool          `json:"initial_phase"`
+	ID                     uuid.UUID        `json:"id"`
+	Name                   string           `json:"name"`
+	RequiredInputMetaData  []MetaTypeObject `json:"required_input_meta_data"`
+	ProvidedOutputMetaData []MetaTypeObject `json:"provided_output_meta_data"`
+	InitialPhase           bool             `json:"initial_phase"`
 }
 
 func GetCoursePhaseTypeDTOFromDBModel(model db.CoursePhaseType) (CoursePhaseType, error) {
-	inputMetaData, err := meta.GetMetaDataDTOFromDBModel(model.RequiredInputMetaData)
+	inputMetaData, err := getMetaTypeArrayDTOFromDBModel(model.RequiredInputMetaData)
 	if err != nil {
 		return CoursePhaseType{}, err
 	}
 
-	outputMetaData, err := meta.GetMetaDataDTOFromDBModel(model.ProvidedOutputMetaData)
+	outputMetaData, err := getMetaTypeArrayDTOFromDBModel(model.ProvidedOutputMetaData)
 	if err != nil {
 		return CoursePhaseType{}, err
 	}
---
+++ server/coursePhaseType/coursePhaseTypeDTO/meta_type.go
@@ -0,0 +1,32 @@
+package coursePhaseTypeDTO
+
+import (
+	"encoding/json"
+	"errors"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type MetaTypeObject struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+	// Add other fields as needed
+}
+
+func getMetaTypeArrayDTOFromDBModel(metaDataBytes []byte) ([]MetaTypeObject, error) {
+	var metaDataArray []MetaTypeObject
+
+	if len(metaDataBytes) == 0 {
+		// Return an empty slice instead of nil to avoid potential nil slice issues
+		return []MetaTypeObject{}, nil
+	}
+
+	err := json.Unmarshal(metaDataBytes, &metaDataArray)
+	if err != nil {
+		errorMessage := "failed to parse meta data array"
+		log.Error(errorMessage, err)
+		err = errors.New(errorMessage)
+	}
+
+	return metaDataArray, err
+}
