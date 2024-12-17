2cd06b7cac05c6173ecd4b37429a93550a97c030
Stefan Niclas Heun <niclas.heun@tum.de>
2024-12-17 16:00:33 +0100

    reintegrating tests

--- server/coursePhase/validation_test.go
+++ server/coursePhase/validation_test.go
@@ -9,70 +9,70 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// func TestValidateCreateCoursePhase(t *testing.T) {
-// 	tests := []struct {
-// 		name          string
-// 		input         coursePhaseDTO.CreateCoursePhase
-// 		expectedError string
-// 	}{
-// 		{
-// 			name: "valid course phase",
-// 			input: coursePhaseDTO.CreateCoursePhase{
-// 				CourseID:          uuid.New(),
-// 				Name:              "Phase 1",
-// 				IsInitialPhase:    true,
-// 				MetaData:          meta.MetaData{"key": "value"},
-// 				CoursePhaseTypeID: uuid.New(),
-// 			},
-// 			expectedError: "",
-// 		},
-// 		{
-// 			name: "missing name",
-// 			input: coursePhaseDTO.CreateCoursePhase{
-// 				CourseID:          uuid.New(),
-// 				Name:              "",
-// 				IsInitialPhase:    false,
-// 				MetaData:          meta.MetaData{"key": "value"},
-// 				CoursePhaseTypeID: uuid.New(),
-// 			},
-// 			expectedError: "course phase name is required",
-// 		},
-// 		{
-// 			name: "missing course ID",
-// 			input: coursePhaseDTO.CreateCoursePhase{
-// 				CourseID:          uuid.Nil,
-// 				Name:              "Phase 1",
-// 				IsInitialPhase:    true,
-// 				MetaData:          meta.MetaData{"key": "value"},
-// 				CoursePhaseTypeID: uuid.New(),
-// 			},
-// 			expectedError: "course id is required",
-// 		},
-// 		{
-// 			name: "missing name and course ID",
-// 			input: coursePhaseDTO.CreateCoursePhase{
-// 				CourseID:          uuid.Nil,
-// 				Name:              "",
-// 				IsInitialPhase:    false,
-// 				MetaData:          meta.MetaData{},
-// 				CoursePhaseTypeID: uuid.New(),
-// 			},
-// 			expectedError: "course phase name is required",
-// 		},
-// 	}
+func TestValidateCreateCoursePhase(t *testing.T) {
+	tests := []struct {
+		name          string
+		input         coursePhaseDTO.CreateCoursePhase
+		expectedError string
+	}{
+		{
+			name: "valid course phase",
+			input: coursePhaseDTO.CreateCoursePhase{
+				CourseID:          uuid.New(),
+				Name:              "Phase 1",
+				IsInitialPhase:    true,
+				MetaData:          meta.MetaData{"key": "value"},
+				CoursePhaseTypeID: uuid.New(),
+			},
+			expectedError: "",
+		},
+		{
+			name: "missing name",
+			input: coursePhaseDTO.CreateCoursePhase{
+				CourseID:          uuid.New(),
+				Name:              "",
+				IsInitialPhase:    false,
+				MetaData:          meta.MetaData{"key": "value"},
+				CoursePhaseTypeID: uuid.New(),
+			},
+			expectedError: "course phase name is required",
+		},
+		{
+			name: "missing course ID",
+			input: coursePhaseDTO.CreateCoursePhase{
+				CourseID:          uuid.Nil,
+				Name:              "Phase 1",
+				IsInitialPhase:    true,
+				MetaData:          meta.MetaData{"key": "value"},
+				CoursePhaseTypeID: uuid.New(),
+			},
+			expectedError: "course id is required",
+		},
+		{
+			name: "missing name and course ID",
+			input: coursePhaseDTO.CreateCoursePhase{
+				CourseID:          uuid.Nil,
+				Name:              "",
+				IsInitialPhase:    false,
+				MetaData:          meta.MetaData{},
+				CoursePhaseTypeID: uuid.New(),
+			},
+			expectedError: "course phase name is required",
+		},
+	}
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			err := validateCreateCoursePhase(tt.input)
-// 			if tt.expectedError == "" {
-// 				assert.NoError(t, err)
-// 			} else {
-// 				assert.Error(t, err)
-// 				assert.EqualError(t, err, tt.expectedError)
-// 			}
-// 		})
-// 	}
-// }
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := validateCreateCoursePhase(tt.input)
+			if tt.expectedError == "" {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+				assert.EqualError(t, err, tt.expectedError)
+			}
+		})
+	}
+}
 
 func TestValidateUpdateCoursePhase(t *testing.T) {
 	tests := []struct {
