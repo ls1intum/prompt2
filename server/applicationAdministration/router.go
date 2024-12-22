cfec519ce8880214399048cfca5a815b6cd131e6
Stefan Niclas Heun <niclas.heun@tum.de>
2024-12-22 08:48:02 +0100

    getting application form endpoint

---
+++ server/applicationAdministration/router.go
@@ -0,0 +1,52 @@
+package applicationAdministration
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/niclasheun/prompt2.0/keycloak"
+	log "github.com/sirupsen/logrus"
+)
+
+// TBD: I will postpone this till I write the client side.
+// currently it is too much guess work which endpoints I will need.
+func setupApplicationRouter(router *gin.RouterGroup, authMiddleware func() gin.HandlerFunc, permissionIDMiddleware func(allowedRoles ...string) gin.HandlerFunc) {
+	application := router.Group("/applications", authMiddleware())
+
+	// Application Form Endpoints
+	application.GET("/:coursePhaseID/form", permissionIDMiddleware(keycloak.CourseLecturer, keycloak.CourseEditor), getApplicationForm)
+	application.PUT("/:coursePhaseID/form", permissionIDMiddleware(keycloak.CourseLecturer), updateApplicationForm)
+
+	// Application Endpoints
+	// application.GET("/:coursePhaseID/applications", permissionIDMiddleware("CourseLecturer", "CourseEditor"), getApplications)
+
+}
+
+func getApplicationForm(c *gin.Context) {
+	// TODO
+	coursePhaseId, err := uuid.Parse(c.Param("coursePhaseID"))
+	if err != nil {
+		handleError(c, http.StatusBadRequest, err)
+		return
+	}
+
+	applicationForm, err := GetApplicationForm(c, coursePhaseId)
+	if err != nil {
+		log.Error(err)
+		handleError(c, http.StatusInternalServerError, errors.New("could not get application form"))
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, applicationForm)
+
+}
+
+func updateApplicationForm(c *gin.Context) {
+	// TODO
+}
+
+func handleError(c *gin.Context, statusCode int, err error) {
+	c.JSON(statusCode, gin.H{"error": err.Error()})
+}
---
+++ server/applicationAdministration/service.go
@@ -0,0 +1,43 @@
+package applicationAdministration
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/niclasheun/prompt2.0/applicationAdministration/applicationDTO"
+	db "github.com/niclasheun/prompt2.0/db/sqlc"
+)
+
+type ApplicationServiceSingleton struct {
+	queries db.Queries
+	conn    *pgxpool.Pool
+}
+
+var ApplicationService *ApplicationServiceSingleton
+
+// TODO
+func GetApplicationForm(ctx context.Context, coursePhaseID uuid.UUID) (applicationDTO.Form, error) {
+	ctxWithTimeout, cancel := db.GetTimeoutContext(ctx)
+	defer cancel()
+
+	applicationQuestionsText, err := ApplicationService.queries.GetApplicationQuestionsTextForCoursePhase(ctxWithTimeout, coursePhaseID)
+	if err != nil {
+		return applicationDTO.Form{}, err
+	}
+
+	applicationQuestionsMultiSelect, err := ApplicationService.queries.GetApplicationQuestionsMultiSelectForCoursePhase(ctxWithTimeout, coursePhaseID)
+
+	applicationFormDTO := applicationDTO.GetFormDTOFromDBModel(applicationQuestionsText, applicationQuestionsMultiSelect)
+
+	return applicationFormDTO, nil
+}
+
+func UpdateApplicationForm() {
+
+}
+
+// TODO
+func GetApplication() {
+
+}
