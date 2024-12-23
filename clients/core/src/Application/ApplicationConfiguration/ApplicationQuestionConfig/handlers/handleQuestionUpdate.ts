860d10c7e39ca391ca81683b4cd39dd8c1f3ae19
Stefan Niclas Heun <niclas.heun@tum.de>
2024-12-23 08:52:48 +0100

    making it a bit cleaner

--- clients/core/src/Application/ApplicationConfiguration/ApplicationQuestionConfig/handlers/handleQuestionUpdate.ts
+++ clients/core/src/Application/ApplicationConfiguration/ApplicationQuestionConfig/handlers/handleQuestionUpdate.ts
@@ -9,35 +9,7 @@ export const handleQuestionUpdate = (
 ) => {
   setApplicationQuestions((prev) => {
     // save it in the right order to be able to use stringify comparison
-    let updatedSanitizedQuestion: ApplicationQuestionText | ApplicationQuestionMultiSelect
-    if ('options' in updatedQuestion) {
-      updatedSanitizedQuestion = {
-        id: updatedQuestion.id,
-        course_phase_id: updatedQuestion.course_phase_id,
-        title: updatedQuestion.title,
-        description: updatedQuestion.description,
-        placeholder: updatedQuestion.placeholder,
-        error_message: updatedQuestion.error_message,
-        is_required: updatedQuestion.is_required,
-        min_select: updatedQuestion.min_select,
-        max_select: updatedQuestion.max_select,
-        options: updatedQuestion.options,
-        order_num: updatedQuestion.order_num,
-      }
-    } else {
-      updatedSanitizedQuestion = {
-        id: updatedQuestion.id,
-        course_phase_id: updatedQuestion.course_phase_id,
-        title: updatedQuestion.title,
-        description: updatedQuestion.description,
-        placeholder: updatedQuestion.placeholder,
-        validation_regex: updatedQuestion.validation_regex,
-        error_message: updatedQuestion.error_message,
-        is_required: updatedQuestion.is_required,
-        allowed_length: updatedQuestion.allowed_length,
-        order_num: updatedQuestion.order_num,
-      }
-    }
-    return prev.map((q) => (q.id === updatedSanitizedQuestion.id ? updatedSanitizedQuestion : q))
+
+    return prev.map((q) => (q.id === updatedQuestion.id ? updatedQuestion : q))
   })
 }
