89e2e0ac3447f045a2738c4c81557f04c000824b
Stefan Niclas Heun <niclas.heun@tum.de>
2024-12-26 21:01:28 +0100

    interface for application strcture

--- clients/core/src/Application/ApplicationForm.tsx
+++ clients/core/src/Application/ApplicationForm.tsx
@@ -1,18 +1,43 @@
 import { Card, CardTitle } from '@/components/ui/card'
 import { ApplicationQuestionMultiSelect } from '@/interfaces/application_question_multi_select'
 import { ApplicationQuestionText } from '@/interfaces/application_question_text'
+import {
+  ApplicationAnswerText,
+  CreateApplicationAnswerText,
+} from '@/interfaces/application_answer_text'
+import {
+  ApplicationAnswerMultiSelect,
+  CreateApplicationAnswerMultiSelect,
+} from '@/interfaces/application_answer_multi_select'
+import { Student } from '@/interfaces/student'
+import { useState } from 'react'
 
 interface ApplicationFormProps {
   questionsText: ApplicationQuestionText[]
   questionsMultiSelect: ApplicationQuestionMultiSelect[]
+  initialAnswersText?: ApplicationAnswerText[]
+  initialAnswersMultiSelect?: ApplicationAnswerMultiSelect[]
+  student?: Student
   onSubmit: () => void
 }
 
 export const ApplicationForm = ({
   questionsText,
   questionsMultiSelect,
+  initialAnswersMultiSelect,
+  initialAnswersText,
+  student,
   onSubmit,
 }: ApplicationFormProps): JSX.Element => {
+  const [answersMultiSelect, setAnswersMultiSelect] = useState<
+    (CreateApplicationAnswerMultiSelect | ApplicationAnswerMultiSelect)[]
+  >(initialAnswersMultiSelect ?? [])
+  const [answersText, setAnswersText] = useState<
+    (CreateApplicationAnswerText | ApplicationAnswerText)[]
+  >(initialAnswersText ?? [])
+
+  const [studentData, setStudentData] = useState<Student | undefined>(student)
+
   return (
     <div>
       <Card>
