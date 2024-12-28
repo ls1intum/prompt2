1dfe63b1a4697404c9175ad9bc78f03f36c8752a
Stefan Niclas Heun <niclas.heun@tum.de>
2024-12-28 20:45:50 +0100

    some tailwind cleanup

--- clients/core/tailwind.config.js
+++ clients/core/tailwind.config.js
@@ -2,8 +2,4 @@ import sharedConfig from '../shared_library/tailwind.config.js'
 
 /** @type {import('tailwindcss').Config} */
 export const presets = [sharedConfig]
-export const content = [
-  'src/**/*.{ts,tsx}',
-  '../shared_library/components/**/*.{ts,tsx}',
-  '../shared_library/components/minimal-tiptap**/*.{ts,tsx}',
-]
+export const content = ['src/**/*.{ts,tsx}', '../shared_library/components/**/*.{ts,tsx}']
