942e61fcadcb7edb808c0d9757a1825af2ca7151
Stefan Niclas Heun <niclas.heun@tum.de>
2024-12-29 15:55:45 +0100

    adding first simple table

--- clients/core/src/ApplicationAdministration/ApplicationAssessment/ApplicationTable.tsx
+++ clients/core/src/ApplicationAdministration/ApplicationAssessment/ApplicationTable.tsx
@@ -3,6 +3,18 @@ import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
 import { getCoursePhaseParticipations } from '../../network/queries/getCoursePhaseParticipations'
 
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table'
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { columns } from './components/columns'
+
 export const ApplicationTable = (): JSX.Element => {
   const { phaseId } = useParams<{ phaseId: string }>()
   const {
@@ -15,16 +27,57 @@ export const ApplicationTable = (): JSX.Element => {
     queryFn: () => getCoursePhaseParticipations(phaseId ?? ''),
   })
 
+  const table = useReactTable({
+    data: fetchedParticipations ?? [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  if (isParticipationsPending) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div>
       <h1>Application Table</h1>
-      {isParticipationsPending && <p>Loading...</p>}
-      {fetchedParticipations?.map((participation) => (
-        <div key={participation.id}>
-          <p>{participation.student.first_name}</p>
-          <p>{participation.passed ? 'Passed' : 'Failed'}</p>
-        </div>
-      ))}
+      <div className='rounded-md border'>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className='h-24 text-center'>
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   )
 }
---
+++ clients/core/src/ApplicationAdministration/ApplicationAssessment/components/columns.tsx
@@ -0,0 +1,17 @@
+import { CoursePhaseParticipationWithStudent } from '@/interfaces/course_phase_participation'
+import { ColumnDef } from '@tanstack/react-table'
+
+export const columns: ColumnDef<CoursePhaseParticipationWithStudent>[] = [
+  {
+    accessorKey: 'student.first_name',
+    header: 'First Name',
+  },
+  {
+    accessorKey: 'student.last_name',
+    header: 'Last Name',
+  },
+  {
+    accessorKey: 'student.email',
+    header: 'Email',
+  },
+]
