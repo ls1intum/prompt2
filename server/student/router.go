81c47bc675d5f00c66157daa5b8ee01dd914d7ba
Stefan Niclas Heun <niclas.heun@tum.de>
2025-01-03 08:55:55 +0100

    securing student endpoint

--- server/student/router.go
+++ server/student/router.go
@@ -6,15 +6,16 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"github.com/niclasheun/prompt2.0/keycloak"
 	"github.com/niclasheun/prompt2.0/student/studentDTO"
 )
 
-func setupStudentRouter(router *gin.RouterGroup) {
-	student := router.Group("/students")
-	student.GET("/", getAllStudents)
-	student.GET("/:uuid", getStudentByID)
-	student.POST("/", createStudent)
-	student.PUT("/:uuid", updateStudent)
+func setupStudentRouter(router *gin.RouterGroup, authMiddleware func() gin.HandlerFunc, permissionRoleMiddleware func(allowedRoles ...string) gin.HandlerFunc) {
+	student := router.Group("/students", authMiddleware())
+	student.GET("/", permissionRoleMiddleware(keycloak.PromptAdmin, keycloak.PromptLecturer), getAllStudents)
+	student.GET("/:uuid", permissionRoleMiddleware(keycloak.PromptAdmin, keycloak.PromptLecturer), getStudentByID)
+	student.POST("/", permissionRoleMiddleware(keycloak.PromptAdmin, keycloak.PromptLecturer), createStudent)
+	student.PUT("/:uuid", permissionRoleMiddleware(keycloak.PromptAdmin, keycloak.PromptLecturer), updateStudent)
 }
 
 func getAllStudents(c *gin.Context) {
