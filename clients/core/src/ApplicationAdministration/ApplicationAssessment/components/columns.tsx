19f8091c462b183e00127801f6707d67111bd520
Niclas Heun <niclas.heun@tum.de>
2025-01-14 11:01:25 +0100

    adding score filters

--- clients/core/src/ApplicationAdministration/ApplicationAssessment/components/columns.tsx
+++ clients/core/src/ApplicationAdministration/ApplicationAssessment/components/columns.tsx
@@ -15,6 +15,7 @@ import { Button } from '@/components/ui/button'
 import { Eye, MoreHorizontal, Trash2 } from 'lucide-react'
 import { Checkbox } from '@/components/ui/checkbox'
 import { ApplicationParticipation } from '@/interfaces/application_participations'
+import { numericRangeFilter } from '../utils/numericRangeFilter'
 
 export const columns = (
   onViewApplication: (id: string) => void,
@@ -100,6 +101,11 @@ export const columns = (
       id: 'score',
       accessorKey: 'score',
       header: ({ column }) => <SortableHeader column={column} title='Score' />,
+      filterFn: (row, columnId, filterValue) => {
+        const rowScore = row.getValue<number | null>(columnId) // row.original.score
+        if (!filterValue || typeof filterValue !== 'object') return true
+        return numericRangeFilter(rowScore, filterValue)
+      },
     },
     ...additionalScoreColumns,
     {
