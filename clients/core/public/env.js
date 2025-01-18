1923ab4e93d1337b0f3bcae68ce4fd66bcb2439f
Niclas Heun <niclas.heun@tum.de>
2025-01-18 21:11:49 +0100

    changing answer export to an own object

--- clients/core/public/env.js
+++ clients/core/public/env.js
@@ -1,6 +1,6 @@
 // these are just the defaults, which are overwritten via env.template.js at production
 window.env = {
-    REACT_APP_CORE_HOST: 'http://localhost:8080',
-    REACT_APP_KEYCLOAK_HOST: 'http://localhost:8081',
-    REACT_APP_KEYCLOAK_REALM_NAME: 'prompt',
+    CORE_HOST: 'http://localhost:8080',
+    KEYCLOAK_HOST: 'http://localhost:8081',
+    KEYCLOAK_REALM_NAME: 'prompt',
   }
