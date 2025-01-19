8faf87727ce6b199a4b8b4eac09321966b8ae33d
Niclas Heun <niclas.heun@tum.de>
2025-01-19 10:02:29 +0100

    adding gravatar size support

--- clients/shared_library/lib/getGravatarUrl.ts
+++ clients/shared_library/lib/getGravatarUrl.ts
@@ -1,7 +1,8 @@
 import { sha256 } from 'js-sha256'
 
-export const getGravatarUrl = (email) => {
+export const getGravatarUrl = (email: string, size?: number) => {
+  const requestedSize = size ?? 200
   const hash = sha256(email.trim().toLowerCase())
 
-  return `https://www.gravatar.com/avatar/${hash}?d=identicon&d=404`
+  return `https://www.gravatar.com/avatar/${hash}?d=identicon&d=404&size=${requestedSize}`
 }
