71da109aa3f81815512bbc7b5b86034dab5e7a13
Stefan Niclas Heun <50545621+niclasheun@users.noreply.github.com>
2025-01-23 14:41:02 +0100

    little code refactoring and adding missing delete functionality (#96)

--- clients/core/src/ApplicationAdministration/pages/ApplicationAssessment/components/table/columns.tsx
+++ clients/core/src/ApplicationAdministration/pages/ApplicationAssessment/components/table/columns.tsx
@@ -3,20 +3,11 @@ import translations from '@/lib/translations.json'
 import { SortableHeader } from './SortableHeader'
 import { getStatusBadge } from '../../utils/getStatusBadge'
 import { getGenderString } from '@/interfaces/gender'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import { Button } from '@/components/ui/button'
-import { Eye, MoreHorizontal, Trash2 } from 'lucide-react'
 import { Checkbox } from '@/components/ui/checkbox'
 import { ApplicationParticipation } from '@/interfaces/application_participations'
 import { numericRangeFilter } from '../../utils/numericRangeFilter'
 import { AdditionalScore } from '@/interfaces/additional_score'
+import { ActionMenu } from './menus/ActionMenu'
 
 export const columns = (
   onViewApplication: (id: string) => void,
@@ -142,43 +133,10 @@ export const columns = (
       id: 'actions',
       cell: ({ row }) => {
         return (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button
-                variant='ghost'
-                className='h-8 w-8 p-0 hover:bg-muted focus-visible:ring-1 focus-visible:ring-ring'
-              >
-                <span className='sr-only'>Open menu</span>
-                <MoreHorizontal className='h-4 w-4' />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align='end' className='w-56'>
-              <DropdownMenuLabel className='font-normal'>
-                <span className='font-semibold'>Application Actions</span>
-              </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem
-                onClick={(event) => {
-                  event.stopPropagation()
-                  onViewApplication(row.original.id)
-                }}
-                className='flex items-center cursor-pointer'
-              >
-                <Eye className='mr-2 h-4 w-4' />
-                <span>View application</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem
-                onClick={(event) => {
-                  event.stopPropagation()
-                  onDeleteApplication(row.original.id)
-                }}
-                className='flex items-center cursor-pointer text-destructive focus:text-destructive'
-              >
-                <Trash2 className='mr-2 h-4 w-4' />
-                <span>Delete application</span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <ActionMenu
+            onViewApplication={() => onViewApplication(row.original.id)}
+            onDeleteApplication={() => onDeleteApplication(row.original.id)}
+          />
         )
       },
     },
