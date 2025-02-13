41d9ef8152b5276e57c23c372dc6ec2f9a358ecc
Stefan Niclas Heun <niclas.heun@tum.de>
2025-02-13 14:55:00 +0100

    adding tabs selector to switch between all applications and accepted applications

--- clients/core/src/managementConsole/applicationAdministration/pages/ApplicationLandingPage/ApplicationLandingPage.tsx
+++ clients/core/src/managementConsole/applicationAdministration/pages/ApplicationLandingPage/ApplicationLandingPage.tsx
@@ -1,77 +1,62 @@
-import { CalendarX, MailWarningIcon } from 'lucide-react'
-import { MissingConfig, MissingConfigItem } from '@/components/MissingConfig'
+import { MissingConfig } from '@/components/MissingConfig'
 import { getIsApplicationConfigured } from '../../utils/getApplicationIsConfigured'
-import { useMemo, useState } from 'react'
+import { useState } from 'react'
 import { ApplicationMetaData } from '../../interfaces/applicationMetaData'
-import { useLocation } from 'react-router-dom'
-import { AssessmentDiagram } from './diagrams/AssessmentDiagram'
-import { ApplicationStatusCard } from './diagrams/ApplicationStatusCard'
-import { ApplicationGenderDiagram } from './diagrams/ApplicationGenderDiagram'
-import { ApplicationStudyBackgroundDiagram } from './diagrams/ApplicationStudyBackgroundDiagram'
-import { ApplicationStudySemesterDiagram } from './diagrams/ApplicationStudySemesterDiagram'
 import { ManagementPageHeader } from '@/components/ManagementPageHeader'
 import { useParseApplicationMetaData } from '../../hooks/useParseApplicationMetaData'
 import { useApplicationStore } from '../../zustand/useApplicationStore'
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs'
+import { PassStatus } from '@tumaet/prompt-shared-state'
+import { useLocation } from 'react-router-dom'
+import { useMissingConfigs } from './hooks/useMissingConfig'
 import { useHideMailingWarning } from './hooks/useHideMailingWarning'
+import { ApplicationDashboard } from './components/ApplicationDashboard'
 
 export const ApplicationLandingPage = (): JSX.Element => {
   const [applicationMetaData, setApplicationMetaData] = useState<ApplicationMetaData | null>(null)
-  const path = useLocation().pathname
+  const { pathname } = useLocation()
   const { coursePhase, participations } = useApplicationStore()
+  const { hideMailingWarning } = useHideMailingWarning()
+  const missingConfigs = useMissingConfigs(
+    applicationMetaData,
+    coursePhase,
+    pathname,
+    hideMailingWarning,
+  )
 
   useParseApplicationMetaData(coursePhase, setApplicationMetaData)
-  const { hideMailingWarning } = useHideMailingWarning()
+  const acceptedApplications = participations.filter((app) => app.passStatus === PassStatus.PASSED)
 
-  const missingConfigs: MissingConfigItem[] = useMemo(() => {
-    const missingConfigItems: MissingConfigItem[] = []
-    if (!getIsApplicationConfigured(applicationMetaData)) {
-      missingConfigItems.push({
-        title: 'Application Phase Deadlines',
-        icon: CalendarX,
-        link: `${path}/configuration`,
-      })
-    }
-    if (
-      coursePhase?.restrictedData?.mailingSettings === undefined &&
-      !coursePhase?.restrictedData?.hideMailingWarning
-    ) {
-      missingConfigItems.push({
-        title: 'Application Mailing Settings',
-        description: `This application phase has no mailing settings configured.
-          If you do not want to send mails, you can hide this warning.`,
-        icon: MailWarningIcon,
-        link: `${path}/mailing`,
-        hide: hideMailingWarning,
-      })
-    }
-    return missingConfigItems
-  }, [
-    applicationMetaData,
-    coursePhase?.restrictedData?.hideMailingWarning,
-    coursePhase?.restrictedData?.mailingSettings,
-    hideMailingWarning,
-    path,
-  ])
+  const isApplicationConfigured = getIsApplicationConfigured(applicationMetaData)
 
   return (
     <div>
-      <ManagementPageHeader>Application Administration</ManagementPageHeader>
-
-      <>
+      <Tabs defaultValue='all' className='mb-6'>
+        <div className='flex flex-col sm:flex-row sm:justify-between mb-6'>
+          <ManagementPageHeader>Application Administration</ManagementPageHeader>
+          <TabsList className='inline-flex items-end mt-4 sm:mt-0 w-fit'>
+            <TabsTrigger value='all'>All Applications</TabsTrigger>
+            <TabsTrigger value='accepted'>Accepted Applications</TabsTrigger>
+          </TabsList>
+        </div>
         <MissingConfig elements={missingConfigs} />
-        <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3 mb-6'>
-          <ApplicationStatusCard
+
+        <TabsContent value='all'>
+          <ApplicationDashboard
+            applications={participations}
             applicationMetaData={applicationMetaData}
-            applicationPhaseIsConfigured={getIsApplicationConfigured(applicationMetaData)}
+            isApplicationConfigured={isApplicationConfigured}
           />
-          <AssessmentDiagram applications={participations ?? []} />
-          <ApplicationGenderDiagram applications={participations ?? []} />
-        </div>
-        <div className='grid gap-6 md:grid-cols-1 lg:grid-cols-2 mb-6'>
-          <ApplicationStudyBackgroundDiagram applications={participations ?? []} />
-          <ApplicationStudySemesterDiagram applications={participations ?? []} />
-        </div>
-      </>
+        </TabsContent>
+
+        <TabsContent value='accepted'>
+          <ApplicationDashboard
+            applications={acceptedApplications}
+            applicationMetaData={applicationMetaData}
+            isApplicationConfigured={isApplicationConfigured}
+          />
+        </TabsContent>
+      </Tabs>
     </div>
   )
 }
