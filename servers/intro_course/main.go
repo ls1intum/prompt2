c5a869f716ef40a6c0cd86bda72098e2ae261465
Stefan Niclas Heun <niclas.heun@tum.de>
2025-02-16 18:54:59 +0100

    setting up db for intro course

--- servers/intro_course/main.go
+++ servers/intro_course/main.go
@@ -1,46 +1,53 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/ls1intum/prompt2/servers/intro_course/utils"
+	log "github.com/sirupsen/logrus"
 )
 
-// func getDatabaseURL() string {
-// 	dbUser := utils.GetEnv("DB_USER", "prompt-postgres")
-// 	dbPassword := utils.GetEnv("DB_PASSWORD", "prompt-postgres")
-// 	dbHost := utils.GetEnv("DB_HOST", "localhost")
-// 	dbPort := utils.GetEnv("DB_PORT", "5432")
-// 	dbName := utils.GetEnv("DB_NAME", "prompt")
-// 	sslMode := utils.GetEnv("SSL_MODE", "disable")
-// 	timeZone := utils.GetEnv("DB_TIMEZONE", "Europe/Berlin") // Add a timezone parameter
+func getDatabaseURL() string {
+	dbUser := utils.GetEnv("DB_USER", "prompt-postgres")
+	dbPassword := utils.GetEnv("DB_PASSWORD", "prompt-postgres")
+	dbHost := utils.GetEnv("DB_HOST_INTRO_COURSE", "localhost")
+	dbPort := utils.GetEnv("DB_PORT_INTRO_COURSE", "5433")
+	dbName := utils.GetEnv("DB_NAME", "prompt")
+	sslMode := utils.GetEnv("SSL_MODE", "disable")
+	timeZone := utils.GetEnv("DB_TIMEZONE", "Europe/Berlin") // Add a timezone parameter
 
-// 	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s&TimeZone=%s", dbUser, dbPassword, dbHost, dbPort, dbName, sslMode, timeZone)
-// }
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s&TimeZone=%s", dbUser, dbPassword, dbHost, dbPort, dbName, sslMode, timeZone)
+}
 
-// func runMigrations(databaseURL string) {
-// 	cmd := exec.Command("migrate", "-path", "./db/migration", "-database", databaseURL, "up")
-// 	cmd.Stdout = os.Stdout
-// 	cmd.Stderr = os.Stderr
-// 	if err := cmd.Run(); err != nil {
-// 		log.Fatalf("Failed to run migrations: %v", err)
-// 	}
-// }
+func runMigrations(databaseURL string) {
+	cmd := exec.Command("migrate", "-path", "./db/migration", "-database", databaseURL, "up")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Failed to run migrations: %v", err)
+	}
+}
 
 func main() {
 	// establish database connection
-	// databaseURL := getDatabaseURL()
-	// log.Debug("Connecting to database at:", databaseURL)
+	databaseURL := getDatabaseURL()
+	log.Debug("Connecting to database at:", databaseURL)
 
-	// // run migrations
-	// runMigrations(databaseURL)
+	// run migrations
+	runMigrations(databaseURL)
 
-	// // establish db connection
-	// conn, err := pgxpool.New(context.Background(), databaseURL)
-	// if err != nil {
-	// 	log.Fatalf("Unable to create connection pool: %v\n", err)
-	// 	os.Exit(1)
-	// }
-	// defer conn.Close()
+	// establish db connection
+	conn, err := pgxpool.New(context.Background(), databaseURL)
+	if err != nil {
+		log.Fatalf("Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
 
 	// query := db.New(conn)
 
