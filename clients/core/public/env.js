2c0b675412afc6c9333c4183915d4032ac1f30bf
Stefan Niclas Heun <niclas.heun@tum.de>
2025-03-10 12:23:18 +0100

    finish the developer form entry

--- clients/core/public/env.js
+++ clients/core/public/env.js
@@ -1,6 +1,7 @@
 // these are just the defaults, which are overwritten via env.template.js at production
 window.env = {
     CORE_HOST: 'http://localhost:8080',
+    INTRO_COURSE_HOST: 'http://localhost:8082',
     KEYCLOAK_HOST: 'http://localhost:8081',
     KEYCLOAK_REALM_NAME: 'prompt',
     CHAIR_NAME_LONG: 'TUM Research Group for Applied Education Technologies',
