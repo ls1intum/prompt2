267ac9d81a58911f954fc5003fd25a55fece5145
Maximilian Rapp <maximilian.rapp@tum.de>
2025-03-11 19:34:00 +0100

    fix routes

--- clients/devops_challenge_component/routes/index.tsx
+++ clients/devops_challenge_component/routes/index.tsx
@@ -1,8 +1,8 @@
-import OverviewPage from 'template_component/src/OverviewPage'
-import SettingsPage from 'template_component/src/SettingsPage'
 import { ExtendedRouteObject } from '@/interfaces/extendedRouteObject'
 import { Role } from '@tumaet/prompt-shared-state'
 import GitHubPage from '../src/devops_challenge/pages/GitHub/GitHubPage'
+import OverviewPage from 'src/devops_challenge/pages/Overview/OverviewPage'
+import SettingsPage from 'src/devops_challenge/pages/SettingsPage/SettingsPage'
 
 const routes: ExtendedRouteObject[] = [
   {
