2136ca2fff6b97ee0aabb86b52afa7482c22f96d
Maximilian Rapp <maximilian.rapp@tum.de>
2025-03-11 18:35:32 +0100

    copy template folder

---
+++ clients/assessment_component/src/App.tsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from 'react-router-dom'
+import { Settings } from 'lucide-react'
+import { OverviewPage } from './OverviewPage'
+import { SettingsPage } from './SettingsPage'
+
+export const App = (): JSX.Element => {
+  return <h1>Hi</h1>
+}
+
+export default App
