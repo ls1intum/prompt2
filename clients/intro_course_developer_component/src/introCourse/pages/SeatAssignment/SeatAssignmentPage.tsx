96cb24d80f26ed583697c34b07ed70da21afc73e
Stefan Niclas Heun <niclas.heun@tum.de>
2025-03-12 16:00:33 +0100

    data fetching

--- clients/intro_course_developer_component/src/introCourse/pages/SeatAssignment/SeatAssignmentPage.tsx
+++ clients/intro_course_developer_component/src/introCourse/pages/SeatAssignment/SeatAssignmentPage.tsx
@@ -1,22 +1,71 @@
+import { ErrorPage } from '@/components/ErrorPage'
 import { ManagementPageHeader } from '@/components/ManagementPageHeader'
+import { getCoursePhaseParticipations } from '@/network/queries/getCoursePhaseParticipations'
 import { useQuery } from '@tanstack/react-query'
+import { CoursePhaseParticipationsWithResolution } from '@tumaet/prompt-shared-state'
+import { Loader2 } from 'lucide-react'
 import { useParams } from 'react-router-dom'
+import { DeveloperProfile } from 'src/introCourse/interfaces/DeveloperProfile'
 import { Tutor } from 'src/introCourse/interfaces/Tutor'
-import { getAllTutors } from 'src/introCourse/network/queries/getAllTutors'
+import { getAllDeveloperProfiles } from '../../network/queries/getAllDeveloperProfiles'
+import { getAllTutors } from '../../network/queries/getAllTutors'
 
 export const SeatAssignmentPage = (): JSX.Element => {
   const { phaseId } = useParams<{ phaseId: string }>()
 
   const {
     data: tutors,
-    isLoading: isLoading,
+    isPending: isPendingTutors,
     isError: isTutorsLoadingError,
+    refetch: refetchTutors,
   } = useQuery<Tutor[]>({
     queryKey: ['tutors', phaseId],
     queryFn: () => getAllTutors(phaseId ?? ''),
   })
 
-  
+  const {
+    data: coursePhaseParticipations,
+    isPending: isCoursePhaseParticipationsPending,
+    isError: isParticipationsError,
+    refetch: refetchCoursePhaseParticipations,
+  } = useQuery<CoursePhaseParticipationsWithResolution>({
+    queryKey: ['participants', phaseId],
+    queryFn: () => getCoursePhaseParticipations(phaseId ?? ''),
+  })
+
+  const {
+    data: developerProfiles,
+    isPending: isDeveloperProfilesPending,
+    isError: isDeveloperProfileError,
+    refetch: refetchDeveloperProfiles,
+  } = useQuery<DeveloperProfile[]>({
+    queryKey: ['developerProfiles', phaseId],
+    queryFn: () => getAllDeveloperProfiles(phaseId ?? ''),
+  })
+
+  const isPending =
+    isCoursePhaseParticipationsPending || isDeveloperProfilesPending || isPendingTutors
+  const isError = isParticipationsError || isDeveloperProfileError || isTutorsLoadingError
+
+  if (isPending) {
+    return (
+      <div className='flex justify-center items-center flex-grow'>
+        <Loader2 className='h-12 w-12 animate-spin text-primary' />
+      </div>
+    )
+  }
+
+  if (isError) {
+    return (
+      <ErrorPage
+        onRetry={() => {
+          refetchCoursePhaseParticipations()
+          refetchDeveloperProfiles()
+          refetchTutors()
+        }}
+      />
+    )
+  }
 
   return (
     <div>
