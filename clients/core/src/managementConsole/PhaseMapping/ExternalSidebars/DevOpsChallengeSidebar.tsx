ab0a876ad66211b76b439815e3035bde4670cf63
Maximilian Rapp <53191688+rappm@users.noreply.github.com>
2025-03-13 19:03:37 +0100

    `DevOps challenge` Initialize new component (#252)
    * create challenge component
    
    Co-authored-by: Mathilde Hagl <mathildeshagl@users.noreply.github.com>
    
    * rename to devops challenge component
    
    Co-authored-by: Mathilde Hagl <mathildeshagl@users.noreply.github.com>
    
    * adding DevOpsChallenge (hard coded) (#251)
    
    * fix missing renames
    
    * add devops challenge image tag to build-and-push workflow
    
    * fix feedback
    
    * 3. deploy new component
    
    * fix path in github action
    
    * fix routes
    
    * finally right path
    
    * Update clients/devops_challenge_component/public/template.html
    
    Co-authored-by: Matthias Linhuber <github@linhuber.org>
    
    * fix build and push script
    
    * fixing missing assignment of error
    
    * fixing version number and names
    
    ---------
    
    Co-authored-by: Mathilde Hagl <mathildeshagl@users.noreply.github.com>
    Co-authored-by: Stefan Niclas Heun <50545621+niclasheun@users.noreply.github.com>
    Co-authored-by: Matthias Linhuber <github@linhuber.org>
    Co-authored-by: Stefan Niclas Heun <niclas.heun@tum.de>

---
+++ clients/core/src/managementConsole/PhaseMapping/ExternalSidebars/DevOpsChallengeSidebar.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { DisabledSidebarMenuItem } from '../../layout/Sidebar/InsideSidebar/components/DisabledSidebarMenuItem'
+import { SidebarMenuItemProps } from '@/interfaces/sidebar'
+import { ExternalSidebarComponent } from './ExternalSidebar'
+
+interface DevOpsChallengeSidebarProps {
+  rootPath: string
+  title?: string
+  coursePhaseID: string
+}
+
+export const DevOpsChallengeSidebar = React.lazy(() =>
+  import('devops_challenge_component/sidebar')
+    .then((module): { default: React.FC<DevOpsChallengeSidebarProps> } => ({
+      default: ({ title, rootPath, coursePhaseID }) => {
+        const sidebarElement: SidebarMenuItemProps = module.default || {}
+        return (
+          <ExternalSidebarComponent
+            title={title}
+            rootPath={rootPath}
+            sidebarElement={sidebarElement}
+            coursePhaseID={coursePhaseID}
+          />
+        )
+      },
+    }))
+    .catch((): { default: React.FC } => ({
+      default: () => {
+        console.warn('Failed to load challenge sidebar')
+        return <DisabledSidebarMenuItem title={'DevOpsChallenge Not Available'} />
+      },
+    })),
+)
