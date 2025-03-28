0bc17c26b4f281d42d2bce12cd1e904593af5868
Mathilde Hagl <mathilde.s.hagl@tum.de>
2025-03-28 11:15:13 +0100

    Refactor getPassedStudentsCount function for improved readability

--- clients/devops_challenge_component/src/devops_challenge/network/queries/getPassedStudentsCount.ts
+++ clients/devops_challenge_component/src/devops_challenge/network/queries/getPassedStudentsCount.ts
@@ -1,8 +1,6 @@
 import { devOpsChallengeAxiosInstance } from '../devOpsChallengeServerConfig'
 
-export const getPassedStudentsCount = async (
-  coursePhaseID: string,
-): Promise<number> => {
+export const getPassedStudentsCount = async (coursePhaseID: string): Promise<number> => {
   try {
     const response = await devOpsChallengeAxiosInstance.get(`/${coursePhaseID}/passed`)
     return response.data.passedStudents
