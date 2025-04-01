5b8dee5e091c76f85e9224c9060744362e23cf8a
Maximilian Rapp <maximilian.rapp@tum.de>
2025-04-01 11:23:43 +0200

    replace all Ids with IDs

--- servers/assessment/assessments/assessmentDTO/createAssessmentRequest.go
+++ servers/assessment/assessments/assessmentDTO/createAssessmentRequest.go
@@ -4,9 +4,9 @@ import "github.com/google/uuid"
 
 // CreateAssessmentRequest is the body used to create an assessment.
 type CreateAssessmentRequest struct {
-	CourseParticipationID uuid.UUID `json:"courseParticipationId"`
-	CoursePhaseID         uuid.UUID `json:"coursePhaseId"`
-	CompetencyID          uuid.UUID `json:"competencyId"`
+	CourseParticipationID uuid.UUID `json:"courseParticipationID"`
+	CoursePhaseID         uuid.UUID `json:"coursePhaseID"`
+	CompetencyID          uuid.UUID `json:"competencyID"`
 	Score                 int16     `json:"score"`
 	Comment               string    `json:"comment,omitempty"`
 }
