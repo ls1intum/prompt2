e76ac502297b529920ce3288b9b3d459e81a6010
Mathilde Hagl <mathilde.s.hagl@tum.de>
2025-04-17 19:48:03 +0200

    Format SQL table creation for allocations: adjust spacing and alignment for improved readability

--- servers/team_allocation/db/migration/0003_allocations.up.sql
+++ servers/team_allocation/db/migration/0003_allocations.up.sql
@@ -1,14 +1,13 @@
 BEGIN;
 
-CREATE TABLE allocations (
-    id uuid NOT NULL PRIMARY KEY,
-    course_participation_id uuid NOT NULL,
-    team_id uuid NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    FOREIGN KEY (team_id) REFERENCES team(id) ON DELETE CASCADE,
-
-    CONSTRAINT unique_allocation_per_student UNIQUE (course_participation_id)
-);
+CREATE TABLE
+    allocations (
+        id uuid NOT NULL PRIMARY KEY,
+        course_participation_id uuid NOT NULL,
+        team_id uuid NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (team_id) REFERENCES team (id) ON DELETE CASCADE,
+        UNIQUE (course_participation_id, team_id)
+    );
 
 COMMIT;
