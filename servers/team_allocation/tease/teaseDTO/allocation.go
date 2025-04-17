4705abc7b1a44f881cd6c096c07732180eac5fb5
Mathilde Hagl <mathilde.s.hagl@tum.de>
2025-04-17 17:38:58 +0200

    Fix JSON tag for ProjectID in Allocation struct to match camelCase convention

--- servers/team_allocation/tease/teaseDTO/allocation.go
+++ servers/team_allocation/tease/teaseDTO/allocation.go
@@ -3,6 +3,6 @@ package teaseDTO
 import "github.com/google/uuid"
 
 type Allocation struct {
-	ProjectID uuid.UUID   `json:"project_id"`
+	ProjectID uuid.UUID   `json:"projectId"`
 	Students  []uuid.UUID `json:"students"`
 }
