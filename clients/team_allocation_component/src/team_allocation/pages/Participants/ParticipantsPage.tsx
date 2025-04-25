9e1e6f7abdc8fad186d9f5ab234d2e46f7e364b1
Stefan Niclas Heun <niclas.heun@tum.de>
2025-04-25 11:10:39 +0200

    adding participants page

---
+++ clients/team_allocation_component/src/team_allocation/pages/Participants/ParticipantsPage.tsx
@@ -0,0 +1,9 @@
+import { ManagementPageHeader } from '@/components/ManagementPageHeader'
+
+export const ParticipantsPage = (): JSX.Element => {
+  return (
+    <div>
+      <ManagementPageHeader>Team Allocation Participants</ManagementPageHeader>
+    </div>
+  )
+}
