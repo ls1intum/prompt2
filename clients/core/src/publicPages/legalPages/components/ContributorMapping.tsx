eabe7ac53ecaef2b04859a430874977ae165bc5b
Mathilde Hagl <mathilde.s.hagl@tum.de>
2025-04-28 14:15:00 +0200

    add contributors

--- clients/core/src/publicPages/legalPages/components/ContributorMapping.tsx
+++ clients/core/src/publicPages/legalPages/components/ContributorMapping.tsx
@@ -5,21 +5,49 @@ export const contributorMapping: {
     position: number
   }
 } = {
+  Mtze: {
+    name: 'Matthias Linhuber',
+    contribution: 'Project Manager',
+    position: 1,
+  },
   niclasheun: {
     name: 'Niclas Heun',
     contribution: 'Lead Developer & Architect of PROMPT 2.0',
     position: 2,
-  },
-  Mtze: { name: 'Matthias Linhuber', contribution: 'Project Manager', position: 1 },
-  mathildeshagl: {
-    name: 'Mathilde Hagl',
-    contribution: 'Templating and Task Management',
-    position: 4,
   },
   airelawaleria: {
     name: 'Valeryia Andraichuk',
     contribution: 'Creator of PROMPT - the concept on which PROMPT 2.0 is based',
     position: 3,
   },
-  rappm: { name: 'Maximilian Rapp', contribution: 'Grading', position: 5 },
+  mathildeshagl: {
+    name: 'Mathilde Hagl',
+    contribution: 'Templating',
+    position: 4,
+  },
+  rappm: {
+    name: 'Maximilian Rapp',
+    contribution: 'Grading',
+    position: 5,
+  },
+  phnagy: {
+    name: 'Philipp Nagy',
+    contribution: 'TEASE',
+    position: 6,
+  },
+  robertjndw: {
+    name: 'Robert Jandow',
+    contribution: 'Server-side contributor',
+    position: 7,
+  },
+  bgeisb: {
+    name: 'Benedikt Geisberger',
+    contribution: 'UI/UX Design',
+    position: 8,
+  },
+  maximiliansoelch: {
+    name: 'Maximilian Sölch',
+    contribution: 'Contributor',
+    position: 9,
+  },
 }
