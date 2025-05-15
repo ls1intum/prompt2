5e9da7b0a95fd92674fdfe4d5c2e30417140c5d5
Mathilde Hagl <mathilde.s.hagl@tum.de>
2025-05-15 13:15:56 +0200

    Fix JSON key for participation in CoursePhaseParticipationWithResolution DTO

--- servers/core/coursePhase/coursePhaseParticipation/coursePhaseParticipationDTO/get_participation_with_resolution.go
+++ servers/core/coursePhase/coursePhaseParticipation/coursePhaseParticipationDTO/get_participation_with_resolution.go
@@ -3,6 +3,6 @@ package coursePhaseParticipationDTO
 import "github.com/niclasheun/prompt2.0/coursePhase/resolution/resolutionDTO"
 
 type CoursePhaseParticipationWithResolution struct {
-	Participation GetAllCPPsForCoursePhase   `json:"participations"`
+	Participation GetAllCPPsForCoursePhase   `json:"participation"`
 	Resolutions   []resolutionDTO.Resolution `json:"resolutions"`
 }
