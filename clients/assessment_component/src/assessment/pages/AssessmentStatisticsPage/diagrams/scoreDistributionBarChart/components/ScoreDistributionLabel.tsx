2ef43de1ef683b76bb5db31fe32d18b0fc12d0dc
Maximilian Rapp <maximilian.rapp@tum.de>
2025-05-26 13:25:42 +0200

    refactor: define ScoreDistributionLabelProps interface for better type safety

--- clients/assessment_component/src/assessment/pages/AssessmentStatisticsPage/diagrams/scoreDistributionBarChart/components/ScoreDistributionLabel.tsx
+++ clients/assessment_component/src/assessment/pages/AssessmentStatisticsPage/diagrams/scoreDistributionBarChart/components/ScoreDistributionLabel.tsx
@@ -1,9 +1,17 @@
 import { ScoreLevel } from '../../../../../interfaces/scoreLevel'
 
-export const ScoreDistributionLabel = (props: any) => {
+interface ScoreDistributionLabelProps {
+  x?: number
+  y?: number
+  width?: number
+  height?: number
+  value?: number
+}
+
+export const ScoreDistributionLabel = (props: ScoreDistributionLabelProps) => {
   const { x = 0, y = 0, width = 0, height = 0, value = 0 } = props
   if (value === 0) return undefined
-  const scale = props.height / (Object.keys(ScoreLevel).length - 1)
+  const scale = height / (Object.keys(ScoreLevel).length - 1)
   const averageY = y + height - (value - 1) * scale
   return (
     <text x={x + width / 2} y={averageY - 6} textAnchor='middle' fontSize={12} fill='#a3a3a3'>
