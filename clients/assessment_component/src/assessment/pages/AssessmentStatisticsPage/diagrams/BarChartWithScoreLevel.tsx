c5bd794c2c890b286023e4a0eaf943dda2e654e6
Maximilian Rapp <maximilian.rapp@tum.de>
2025-05-26 17:17:03 +0200

    refactor: improve BarChartWithScoreLevel component structure and performance

--- clients/assessment_component/src/assessment/pages/AssessmentStatisticsPage/diagrams/BarChartWithScoreLevel.tsx
+++ clients/assessment_component/src/assessment/pages/AssessmentStatisticsPage/diagrams/BarChartWithScoreLevel.tsx
@@ -1,5 +1,6 @@
 import { ChartContainer, ChartTooltip, ChartTooltipContent } from '@tumaet/prompt-ui-components'
 import { BarChart, Bar, LabelList, XAxis, YAxis, Rectangle } from 'recharts'
+import { useMemo } from 'react'
 import { chartConfig } from './utils/chartConfig'
 import { getCornerRadius } from './utils/getCornerRadius'
 import { DataPoint } from '../interfaces/DataPoint'
@@ -8,9 +9,22 @@ interface BarChartWithScoreLevelProps {
   data: DataPoint[]
 }
 
+interface RoundedBarShapeProps {
+  x?: number
+  y?: number
+  width?: number
+  height?: number
+  payload?: DataPoint
+  fill?: string
+  [key: string]: unknown
+}
+
 const createRoundedBarShape = (segmentKey: string, stackKeys?: string[]) => {
-  const RoundedBarShape = (props: any) => {
-    const { x, y, width, height, payload } = props
+  const RoundedBarShape = (props: unknown) => {
+    const { x = 0, y = 0, width = 0, height = 0, payload } = props as RoundedBarShapeProps
+
+    if (!payload) return <Rectangle x={0} y={0} width={0} height={0} />
+
     const radius = getCornerRadius(payload, segmentKey, stackKeys)
     return (
       <Rectangle
@@ -28,6 +42,13 @@ const createRoundedBarShape = (segmentKey: string, stackKeys?: string[]) => {
 }
 
 export const BarChartWithScoreLevel = ({ data }: BarChartWithScoreLevelProps) => {
+  const memoizedBarShapes = useMemo(() => {
+    return ['novice', 'intermediate', 'advanced', 'expert', 'notAssessed'].map((key) => ({
+      key,
+      shape: createRoundedBarShape(key),
+    }))
+  }, [])
+
   return (
     <ChartContainer config={chartConfig} className='mx-auto w-full h-[280px]'>
       <BarChart data={data} margin={{ top: 30, right: 10, bottom: 0, left: 10 }}>
@@ -43,8 +64,8 @@ export const BarChartWithScoreLevel = ({ data }: BarChartWithScoreLevelProps) =>
         <YAxis hide />
         <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
 
-        {['novice', 'intermediate', 'advanced', 'expert', 'notAssessed'].map((key) => (
-          <Bar key={key} dataKey={key} shape={createRoundedBarShape(key)}>
+        {memoizedBarShapes.map(({ key, shape }) => (
+          <Bar key={key} dataKey={key} shape={shape}>
             <LabelList
               dataKey={key}
               position='top'
