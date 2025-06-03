d5f22833f7314b1e6c6331bdb8b481bf2f39f554
Stefan Niclas Heun <50545621+niclasheun@users.noreply.github.com>
2025-06-03 12:04:21 +0200

    General: Adjusting the Header and Footer to the chairs guidelines (#547)
    * adjusted header and footer for logged in view
    
    * fixing unauthenticated header
    
    * adjusted divider color

--- clients/core/src/managementConsole/layout/Sidebar/InsideSidebar/InsideSidebar.tsx
+++ clients/core/src/managementConsole/layout/Sidebar/InsideSidebar/InsideSidebar.tsx
@@ -1,23 +1,25 @@
 import { Sidebar, SidebarContent, SidebarHeader } from '@tumaet/prompt-ui-components'
-import { useCourseStore } from '@tumaet/prompt-shared-state'
-import { useLocation, useParams } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { InsideCourseSidebar } from './InsideCourseSidebar'
 import { InsideGeneralSidebar } from './InsideGeneralSidebar'
+import packageJSON from '../../../../../package.json'
 
 export const InsideSidebar = (): JSX.Element => {
-  const { courses } = useCourseStore()
-  const { courseId } = useParams<{ courseId: string }>()
-  const currentCourse = courses.find((course) => course.id === courseId)
+  const version = packageJSON.version
 
   // set the correct header
   const location = useLocation()
   const isCourseSidebar = location.pathname.startsWith('/management/course')
-  const headerName = isCourseSidebar ? currentCourse?.name || 'Unknown Course' : 'General'
 
   return (
     <Sidebar collapsible='none' className='flex'>
-      <SidebarHeader className='flex h-14 border-b justify-center'>
-        <div className='text-base font-medium text-foreground'>{headerName}</div>
+      <SidebarHeader className='flex h-14 border-b justify-center items-center'>
+        <div className='relative flex items-baseline'>
+          <span className='text-lg font-extrabold tracking-wide text-primary drop-shadow-sm'>
+            PROMPT
+          </span>
+          <span className='ml-1 text-xs font-normal text-gray-400'>{version}</span>
+        </div>
       </SidebarHeader>
       <SidebarContent>
         {isCourseSidebar ? <InsideCourseSidebar /> : <InsideGeneralSidebar />}
