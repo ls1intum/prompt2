5d771053d8340eb9f0117609f474132d62bc39ef
Maximilian Rapp <maximilian.rapp@tum.de>
2025-06-21 18:10:04 +0200

    refactor AssessmentOverviewPage to remove unused score levels fetching logic and improve code clarity

--- clients/assessment_component/src/assessment/pages/AssessmentOverviewPage/AssessmentOverviewPage.tsx
+++ clients/assessment_component/src/assessment/pages/AssessmentOverviewPage/AssessmentOverviewPage.tsx
@@ -1,15 +1,13 @@
-import { Loader2 } from 'lucide-react'
-import { useNavigate, useLocation } from 'react-router-dom'
-import {
-  ErrorPage,
-  ManagementPageHeader,
-  useCustomElementWidth,
-} from '@tumaet/prompt-ui-components'
-import { CoursePhaseParticipationsTablePage } from '@/components/pages/CoursePhaseParticpationsTable/CoursePhaseParticipationsTablePage'
-import { useGetAllScoreLevels } from '../hooks/useGetAllScoreLevels'
-import { useParticipationStore } from '../../zustand/useParticipationStore'
-import { StudentScoreBadge } from '../components/StudentScoreBadge'
 import { useMemo } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
+
+import { ManagementPageHeader, useCustomElementWidth } from '@tumaet/prompt-ui-components'
+import { CoursePhaseParticipationsTablePage } from '@/components/pages/CoursePhaseParticpationsTable/CoursePhaseParticipationsTablePage'
+
+import { StudentScoreBadge } from '../components/StudentScoreBadge'
+
+import { useParticipationStore } from '../../zustand/useParticipationStore'
+import { useScoreLevelStore } from '../../zustand/useScoreLevelStore'
 
 import { AssessmentDiagram } from '../components/diagrams/AssessmentDiagram'
 import { AssessmentScoreLevelDiagram } from '../components/diagrams/AssessmentScoreLevelDiagram'
@@ -20,17 +18,7 @@ export const AssessmentOverviewPage = (): JSX.Element => {
   const tableWidth = useCustomElementWidth('table-view')
 
   const { participations } = useParticipationStore()
-
-  const {
-    data: scoreLevels,
-    isPending: isScoreLevelsPending,
-    isError: isScoreLevelsError,
-    refetch: refetchScoreLevels,
-  } = useGetAllScoreLevels()
-
-  const refetch = () => {
-    refetchScoreLevels()
-  }
+  const { scoreLevels } = useScoreLevelStore()
 
   const extraData = useMemo(() => {
     return scoreLevels?.map((scoreLevelWithParticipation) => ({
@@ -38,15 +26,6 @@ export const AssessmentOverviewPage = (): JSX.Element => {
       value: <StudentScoreBadge scoreLevel={scoreLevelWithParticipation.scoreLevel} />,
     }))
   }, [scoreLevels])
-
-  if (isScoreLevelsError)
-    return <ErrorPage onRetry={refetch} description='Could not fetch scoreLevels' />
-  if (isScoreLevelsPending)
-    return (
-      <div className='flex justify-center items-center h-64'>
-        <Loader2 className='h-12 w-12 animate-spin text-primary' />
-      </div>
-    )
 
   return (
     <div id='table-view' className='relative flex flex-col'>
