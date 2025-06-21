82d68afc1bbae2b9f02064da228ff9f0bd796dd4
Maximilian Rapp <maximilian.rapp@tum.de>
2025-06-21 17:52:38 +0200

    fix getDeadline to correctly parse response data as a Date object

--- clients/assessment_component/src/assessment/network/queries/getDeadline.ts
+++ clients/assessment_component/src/assessment/network/queries/getDeadline.ts
@@ -1,7 +1,7 @@
 import { assessmentAxiosInstance } from '../assessmentServerConfig'
 
 export const getDeadline = async (coursePhaseID: string): Promise<Date> => {
-  const response = await assessmentAxiosInstance.get<Date>(
+  const response = await assessmentAxiosInstance.get<string>(
     `assessment/api/course_phase/${coursePhaseID}/config/deadline`,
     {
       headers: {
@@ -9,5 +9,5 @@ export const getDeadline = async (coursePhaseID: string): Promise<Date> => {
       },
     },
   )
-  return response.data
+  return new Date(response.data)
 }
