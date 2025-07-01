3350e76aaf8da2b4560e341fa33189a9a15ab981
Mathilde Hagl <mathilde.s.hagl@tum.de>
2025-07-01 10:56:11 +0200

    Templating: Copy Course with all Core Data (#525)
    * add basic copying functionality (without any config)
    
    * Refactor: Simplify CopyCourse function and add unit test for course copying
    
    * Enhance course copying functionality: generate unique course names and add course phase handling
    
    * Implement CopyCourse functionality: add course variables handling and update course creation logic
    
    * Add CopyCourse UI: implement CopyCourseDialog, mutation, and validation schema
    
    * Refactor CopyCourseDialog: update form field and validation schema to use 'name' instead of 'courseName', change defaultvalues of input field
    
    * Remove CheckCourseExistsByName query
    
    * Add context menu to CourseSidebarItem: implement copy, edit, and delete options
    
    * Add context menu options to CourseSidebarItem: implement copy, edit, and delete functionalities
    
    * Remove handleItemClick function from CourseSidebarItem to streamline context menu interactions
    
    * add comments for clarity
    
    * Implement phase data graph copying in CopyCourse function
    
    * Refactor CopyCourse function to improve phase data graph handling and error messaging
    
    * Remove hardcoded Keycloak client secret for improved security
    
    * Enhance CopyCourse function to include participation data graph copying and improve phase ID mapping validation
    
    * Enhance CopyCourse function to validate all phase ID mappings during data graph copying
    
    * Enhance CopyCourse function to fetch all phases and improve phase ID mapping validation
    
    * make copy.go file and seperate methods for better clarity
    
    * Refactor copyMetaGraphs function to remove unused parameter and improve clarity; update copyCourseInternal to handle DTO mappings correctly.
    
    * Enhance copyDTOs function to include course phase sequence retrieval for improved phase type uniqueness validation
    
    * Refactor copyMetaGraphs function to include query transaction parameter and improve data graph connection updates
    
    * Refactor copyDTOs function to reuse original IDs for participation outputs and inputs, improving data integrity and reducing unnecessary ID generation.
    
    * rename copy function for more clarity
    
    * Fix import paths in copy_course.go to ensure correct package references
    
    * fix: copy correct course when copying from sidebar
    
    * fix: ensure onOpenChange calls onClose correctly in CopyCourseDialog
    
    * fix: update course phase queries to use transaction context
    
    * feat: implement application form copying and add application phase ID retrieval
    
    * fix: update copyApplicationForm to include transaction context and add UpdateApplicationFormHelper for question management
    
    * test: add CopyCourseTestSuite to validate course copying functionality
    
    * refactor: remove TestCopyCourse from CourseRouterTestSuite
    
    * Add testing sql dump and add tests
    
    * remove right-click functionality
    
    * feat: add date range fields to CopyCourse and update related components
    
    * refactor: improve dateRange validation in copyCourseSchema
    
    * address feedback
    
    * Add API documentation for copyCourse endpoint
    
    * add TestCopyCourse (router test) to validate course copying functionality
    
    * Refactor function names
    
    * Remove log info statements
    
    * Rename CopyCourse struct to CopyCourseRequest
    
    * Handle potential errors when retrieving application phase IDs in copyCourseInternal
    
    * Enhance documentation for course copying functions in copy_course.go

---
+++ clients/core/src/network/mutations/copyCourse.ts
@@ -0,0 +1,24 @@
+import { axiosInstance } from '@/network/configService'
+import {
+  CopyCourse,
+  serializeCopyCourse,
+} from '@core/managementConsole/courseOverview/interfaces/copyCourse'
+
+export const copyCourse = async (
+  courseID: string,
+  courseVariables: CopyCourse,
+): Promise<string | undefined> => {
+  try {
+    const serializedCourse = serializeCopyCourse(courseVariables)
+    return (
+      await axiosInstance.post(`/api/courses/${courseID}/copy`, serializedCourse, {
+        headers: {
+          'Content-Type': 'application/json-path+json',
+        },
+      })
+    ).data.id // try to get the id of the copied course
+  } catch (err) {
+    console.error(err)
+    throw err
+  }
+}
