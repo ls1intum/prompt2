5b82081e520f7aa29f2f2e2b3f92643f0adcefb8
Maximilian Rapp <maximilian.rapp@tum.de>
2025-07-07 13:45:24 +0200

    feat: add self and peer evaluation pages and update routes for CourseStudent role access

--- clients/assessment_component/routes/index.tsx
+++ clients/assessment_component/routes/index.tsx
@@ -1,14 +1,32 @@
 import { ExtendedRouteObject } from '@/interfaces/extendedRouteObject'
 import { Role } from '@tumaet/prompt-shared-state'
-import { SettingsPage } from '../src/assessment/pages/SettingsPage/SettingsPage'
+
+import { SelfAndPeerAssessmentDataShell } from '../src/assessment/pages/SelfAndPeerAssessmentDataShell'
+import { SelfAndPeerEvaluationPage } from '../src/assessment/pages/SelfAndPeerEvaluationPage/SelfAndPeerEvaluationPage'
+
+import { AssessmentDataShell } from '../src/assessment/pages/AssessmentDataShell'
 import { AssessmentOverviewPage } from '../src/assessment/pages/AssessmentOverviewPage/AssessmentOverviewPage'
 import { AssessmentPage } from '../src/assessment/pages/AssessmentPage/AssessmentPage'
-import { AssessmentDataShell } from '../src/assessment/pages/AssessmentDataShell'
 import { AssessmentStatisticsPage } from '../src/assessment/pages/AssessmentStatisticsPage/AssessmentStatisticsPage'
+import { SettingsPage } from '../src/assessment/pages/SettingsPage/SettingsPage'
 
 const routes: ExtendedRouteObject[] = [
   {
     path: '',
+    element: (
+      <SelfAndPeerAssessmentDataShell>
+        <SelfAndPeerEvaluationPage />
+      </SelfAndPeerAssessmentDataShell>
+    ),
+    requiredPermissions: [
+      Role.PROMPT_ADMIN,
+      Role.COURSE_LECTURER,
+      Role.COURSE_EDITOR,
+      Role.COURSE_STUDENT,
+    ],
+  },
+  {
+    path: '/overview',
     element: (
       <AssessmentDataShell>
         <AssessmentOverviewPage />
---
+++ clients/assessment_component/src/assessment/pages/SelfAndPeerEvaluationPage/SelfAndPeerEvaluationPage.tsx
@@ -0,0 +1,11 @@
+export const SelfAndPeerEvaluationPage = () => {
+  return (
+    <div className='flex flex-col gap-4'>
+      <h1 className='text-2xl font-bold'>Self and Peer Evaluation</h1>
+      <p className='text-gray-600'>
+        This page allows you to manage self and peer evaluation settings for your assessment.
+      </p>
+      {/* Add components for self and peer evaluation management here */}
+    </div>
+  )
+}
