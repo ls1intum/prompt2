97acb2922c78874e121c0a2139c19afb165a5b2b
Mathilde Hagl <mathilde.s.hagl@tum.de>
2025-07-08 16:20:46 +0200

    Feature: Implement phase copy functionality in TeamAllocationCopyHandler

--- servers/team_allocation/copy/service.go
+++ servers/team_allocation/copy/service.go
@@ -2,6 +2,7 @@ package copy
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
 	promptTypes "github.com/ls1intum/prompt-sdk/promptTypes"
 	db "github.com/ls1intum/prompt2/servers/team_allocation/db/sqlc"
@@ -17,5 +18,22 @@ var CopyServiceSingleton *CopyService
 type TeamAllocationCopyHandler struct{}
 
 func (h *TeamAllocationCopyHandler) HandlePhaseCopy(c *gin.Context, req promptTypes.PhaseCopyRequest) error {
+	skills, err := CopyServiceSingleton.queries.GetSkillsByCoursePhase(c.Request.Context(), req.SourceCoursePhaseID)
+	if err != nil {
+		return err
+	}
+
+	// Copy skills to the new course phase
+	for _, skill := range skills {
+		err := CopyServiceSingleton.queries.CreateSkill(c.Request.Context(), db.CreateSkillParams{
+			ID:            uuid.New(),
+			Name:          skill.Name,
+			CoursePhaseID: req.TargetCoursePhaseID,
+		})
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
