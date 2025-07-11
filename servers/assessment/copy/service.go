35d39a43d6f5161af7360bbe6ca3f08625cee462
Mathilde Hagl <mathilde.s.hagl@tum.de>
2025-07-11 12:44:46 +0200

    Assessment: Refactor HandlePhaseCopy to copy course phase config and remove assessment template logic

--- servers/assessment/copy/service.go
+++ servers/assessment/copy/service.go
@@ -31,72 +31,27 @@ func (h *AssessmentCopyHandler) HandlePhaseCopy(c *gin.Context, req promptTypes.
 
 	qtx := CopyServiceSingleton.queries.WithTx(tx)
 
-	// Copy assessment templates
-	assessmentTemplates, err := qtx.ListAssessmentTemplates(ctx)
+	// Copy course phase config
+	phaseConfig, err := qtx.GetCoursePhaseConfig(ctx, req.SourceCoursePhaseID)
 	if err != nil {
-		log.Error("could not list assessment templates: ", err)
-		return fmt.Errorf("failed to list assessment templates: %w", err)
+		log.Error("could not get course phase config: ", err)
+		return fmt.Errorf("failed to get course phase config: %w", err)
 	}
 
-	for _, template := range assessmentTemplates {
-		err = qtx.CreateAssessmentTemplate(ctx, db.CreateAssessmentTemplateParams{
-			ID:          template.ID,
-			Name:        template.Name,
-			Description: template.Description,
-		})
-		if err != nil {
-			log.Error("could not create assessment template: ", err)
-			return fmt.Errorf("failed to create assessment template: %w", err)
-		}
-	}
-
-	// Copy categories
-	categories, err := qtx.ListCategories(ctx)
+	err = qtx.CreateOrUpdateCoursePhaseConfig(ctx, db.CreateOrUpdateCoursePhaseConfigParams{
+		AssessmentTemplateID:   phaseConfig.AssessmentTemplateID,
+		CoursePhaseID:          req.TargetCoursePhaseID,
+		Deadline:               phaseConfig.Deadline,
+		SelfEvaluationEnabled:  phaseConfig.SelfEvaluationEnabled,
+		SelfEvaluationTemplate: phaseConfig.SelfEvaluationTemplate,
+		SelfEvaluationDeadline: phaseConfig.SelfEvaluationDeadline,
+		PeerEvaluationEnabled:  phaseConfig.PeerEvaluationEnabled,
+		PeerEvaluationTemplate: phaseConfig.PeerEvaluationTemplate,
+		PeerEvaluationDeadline: phaseConfig.PeerEvaluationDeadline,
+	})
 	if err != nil {
-		log.Error("could not list categories: ", err)
-		return fmt.Errorf("failed to list categories: %w", err)
-	}
-
-	for _, category := range categories {
-		err = qtx.CreateCategory(ctx, db.CreateCategoryParams{
-			ID:                   category.ID,
-			Name:                 category.Name,
-			ShortName:            category.ShortName,
-			Description:          category.Description,
-			Weight:               category.Weight,
-			AssessmentTemplateID: category.AssessmentTemplateID,
-		})
-		if err != nil {
-			log.Error("could not create category: ", err)
-			return fmt.Errorf("failed to create category: %w", err)
-		}
-	}
-
-	// Copy competencies
-	competencies, err := qtx.ListCompetencies(ctx)
-	if err != nil {
-		log.Error("could not list competencies: ", err)
-		return fmt.Errorf("failed to list competencies: %w", err)
-	}
-
-	for _, competency := range competencies {
-		err = qtx.CreateCompetency(ctx, db.CreateCompetencyParams{
-			ID:                  competency.ID,
-			CategoryID:          competency.CategoryID,
-			Name:                competency.Name,
-			ShortName:           competency.ShortName,
-			Description:         competency.Description,
-			DescriptionVeryBad:  competency.DescriptionVeryBad,
-			DescriptionBad:      competency.DescriptionBad,
-			DescriptionOk:       competency.DescriptionOk,
-			DescriptionGood:     competency.DescriptionGood,
-			DescriptionVeryGood: competency.DescriptionVeryGood,
-			Weight:              competency.Weight,
-		})
-		if err != nil {
-			log.Error("could not create competency: ", err)
-			return fmt.Errorf("failed to create competency: %w", err)
-		}
+		log.Error("could not create or update course phase config: ", err)
+		return fmt.Errorf("failed to create or update course phase config: %w", err)
 	}
 
 	if err := tx.Commit(ctx); err != nil {
