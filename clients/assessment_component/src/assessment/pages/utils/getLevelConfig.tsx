369e3d4f00399d8c1df709d5f57e54d0602f5501
Maximilian Rapp <maximilian.rapp@tum.de>
2025-07-22 18:52:33 +0200

    Remove redundant comments about border opacity in getLevelConfig

--- clients/assessment_component/src/assessment/pages/utils/getLevelConfig.tsx
+++ clients/assessment_component/src/assessment/pages/utils/getLevelConfig.tsx
@@ -7,7 +7,7 @@ export const getLevelConfig = (level: ScoreLevel, unknown?: boolean) => {
       evaluationTitle: 'Unknown',
       textColor: 'text-gray-700 dark:text-gray-100',
       selectedBg: 'bg-gray-100 dark:bg-gray-600',
-      border: 'border-gray-200 dark:border-gray-200', // Less opaque
+      border: 'border-gray-200 dark:border-gray-200',
     }
   }
 
@@ -18,7 +18,7 @@ export const getLevelConfig = (level: ScoreLevel, unknown?: boolean) => {
         evaluationTitle: 'Strongly Disagree',
         textColor: 'text-red-700 dark:text-red-100',
         selectedBg: 'bg-red-200 dark:bg-red-600',
-        border: 'border-red-200 dark:border-red-200', // Less opaque
+        border: 'border-red-200 dark:border-red-200',
       }
     case ScoreLevel.Bad:
       return {
@@ -26,7 +26,7 @@ export const getLevelConfig = (level: ScoreLevel, unknown?: boolean) => {
         evaluationTitle: 'Disagree',
         textColor: 'text-orange-700 dark:text-orange-100',
         selectedBg: 'bg-orange-200 dark:bg-orange-600',
-        border: 'border-orange-200 dark:border-orange-200', // Less opaque
+        border: 'border-orange-200 dark:border-orange-200',
       }
     case ScoreLevel.Ok:
       return {
@@ -34,7 +34,7 @@ export const getLevelConfig = (level: ScoreLevel, unknown?: boolean) => {
         evaluationTitle: 'Neutral',
         textColor: 'text-yellow-700 dark:text-yellow-100',
         selectedBg: 'bg-yellow-200 dark:bg-yellow-600',
-        border: 'border-yellow-200 dark:border-yellow-200', // Less opaque
+        border: 'border-yellow-200 dark:border-yellow-200',
       }
     case ScoreLevel.Good:
       return {
@@ -42,7 +42,7 @@ export const getLevelConfig = (level: ScoreLevel, unknown?: boolean) => {
         evaluationTitle: 'Agree',
         textColor: 'text-green-700 dark:text-green-100',
         selectedBg: 'bg-green-200 dark:bg-green-600',
-        border: 'border-green-200 dark:border-green-200', // Less opaque
+        border: 'border-green-200 dark:border-green-200',
       }
     case ScoreLevel.VeryGood:
       return {
@@ -50,7 +50,7 @@ export const getLevelConfig = (level: ScoreLevel, unknown?: boolean) => {
         evaluationTitle: 'Strongly Agree',
         textColor: 'text-blue-700 dark:text-blue-100',
         selectedBg: 'bg-blue-200 dark:bg-blue-600',
-        border: 'border-blue-200 dark:border-blue-200', // Less opaque
+        border: 'border-blue-200 dark:border-blue-200',
       }
   }
 }
