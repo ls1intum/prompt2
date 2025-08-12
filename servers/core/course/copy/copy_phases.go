5153644de5d5851a138920751402c0377d616d9e
Mathilde Hagl <mathilde.s.hagl@tum.de>
2025-08-12 11:55:50 +0200

    Refactor: Sanitize restricted data by removing applicationStartDate and applicationEndDate in copyCoursePhases

--- servers/core/course/copy/copy_phases.go
+++ servers/core/course/copy/copy_phases.go
@@ -48,20 +48,19 @@ func copyCoursePhases(c *gin.Context, qtx *db.Queries, sourceID, targetID uuid.U
 			return nil, fmt.Errorf("failed to get course phase by ID %s: %w", oldID, err)
 		}
 
-		restrictedData := phase.RestrictedData
-		if restrictedData["applicationStartDate"] != nil {
-			delete(restrictedData, "applicationStartDate")
+		sanitizedRestrictedData := make(map[string]interface{}, len(phase.RestrictedData))
+		for k, v := range phase.RestrictedData {
+			sanitizedRestrictedData[k] = v
 		}
-		if restrictedData["applicationEndDate"] != nil {
-			delete(restrictedData, "applicationEndDate")
-		}
+		delete(sanitizedRestrictedData, "applicationStartDate")
+		delete(sanitizedRestrictedData, "applicationEndDate")
 
 		newPhase := coursePhaseDTO.CreateCoursePhase{
 			Name:                phase.Name,
 			IsInitialPhase:      phase.IsInitialPhase,
 			CourseID:            targetID,
 			CoursePhaseTypeID:   phase.CoursePhaseTypeID,
-			RestrictedData:      restrictedData,
+			RestrictedData:      sanitizedRestrictedData,
 			StudentReadableData: phase.StudentReadableData,
 		}
 		dbModel, err := newPhase.GetDBModel()
