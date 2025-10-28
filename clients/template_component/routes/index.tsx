d8c8c555b1c6319ce1430ce1712155ddbe4145ac
Mathilde Hagl <mathilde.s.hagl@tum.de>
2025-10-28 14:36:42 +0100

    Add participants and settings pages to template component routes

--- clients/template_component/routes/index.tsx
+++ clients/template_component/routes/index.tsx
@@ -1,8 +1,8 @@
 import { TemplateComponentOverviewPage } from 'template_component/src/OverviewPage'
-import { TemplateComponentSettingsPage } from 'template_component/src/SettingsPage'
+import { TemplateComponentSettingsPage } from 'template_component/src/template_component/pages/SettingsPage'
 import { ExtendedRouteObject } from '@/interfaces/extendedRouteObject'
 import { Role } from '@tumaet/prompt-shared-state'
-import { TemplateComponentParticipantsPage } from 'src/ParticipantsPage'
+import { TemplateComponentParticipantsPage } from 'src/template_component/pages/ParticipantsPage'
 
 const routes: ExtendedRouteObject[] = [
   {
