f4d2d1aa40694d3ebdc6ad10d4eb216b21662663
Mathilde Hagl <mathilde.s.hagl@tum.de>
2025-10-28 14:56:30 +0100

    Fix import path for TemplateComponentParticipantsPage in routes configuration

--- clients/template_component/routes/index.tsx
+++ clients/template_component/routes/index.tsx
@@ -2,7 +2,7 @@ import { TemplateComponentOverviewPage } from 'template_component/src/OverviewPa
 import { TemplateComponentSettingsPage } from 'template_component/src/template_component/pages/SettingsPage'
 import { ExtendedRouteObject } from '@/interfaces/extendedRouteObject'
 import { Role } from '@tumaet/prompt-shared-state'
-import { TemplateComponentParticipantsPage } from 'src/template_component/pages/ParticipantsPage'
+import { TemplateComponentParticipantsPage } from '../src/template_component/pages/ParticipantsPage'
 
 const routes: ExtendedRouteObject[] = [
   {
