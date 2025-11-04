350163823755b1a12facaa30b3baabf14bc60a42
Mathilde Hagl <mathilde.s.hagl@tum.de>
2025-11-04 13:13:11 +0100

    refactor(main.go): move database connection variables outside of getDatabaseURL function

--- servers/template_server/main.go
+++ servers/template_server/main.go
@@ -18,15 +18,15 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func getDatabaseURL() string {
-	dbUser := promptSDK.GetEnv("DB_USER", "prompt-postgres")
-	dbPassword := promptSDK.GetEnv("DB_PASSWORD", "prompt-postgres")
-	dbHost := promptSDK.GetEnv("DB_HOST_TEMPLATE_SERVER", "localhost")
-	dbPort := promptSDK.GetEnv("DB_PORT_TEMPLATE_SERVER", "5437")
-	dbName := promptSDK.GetEnv("DB_NAME", "prompt")
-	sslMode := promptSDK.GetEnv("SSL_MODE", "disable")
-	timeZone := promptSDK.GetEnv("DB_TIMEZONE", "Europe/Berlin") // Add a timezone parameter
+var dbUser string = promptSDK.GetEnv("DB_USER", "prompt-postgres")
+var dbPassword string = promptSDK.GetEnv("DB_PASSWORD", "prompt-postgres")
+var dbHost string = promptSDK.GetEnv("DB_HOST_TEMPLATE_SERVER", "localhost")
+var dbPort string = promptSDK.GetEnv("DB_PORT_TEMPLATE_SERVER", "5437")
+var dbName string = promptSDK.GetEnv("DB_NAME", "prompt")
+var sslMode string = promptSDK.GetEnv("SSL_MODE", "disable")
+var timeZone string = promptSDK.GetEnv("DB_TIMEZONE", "Europe/Berlin") // Add a timezone parameter
 
+func getDatabaseURL() string {
 	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s&TimeZone=%s", dbUser, dbPassword, dbHost, dbPort, dbName, sslMode, timeZone)
 }
 
@@ -58,7 +58,7 @@ func initKeycloak(queries db.Queries) {
 func main() {
 	// establish database connection
 	databaseURL := getDatabaseURL()
-	log.Debugf("Connecting to database at %s", databaseURL)
+	log.Debugf("Connecting to database at host=%s port=%s db=%s user=%s sslmode=%s", dbHost, dbPort, dbName, dbUser, sslMode)
 
 	// run migrations
 	runMigrations(databaseURL)
