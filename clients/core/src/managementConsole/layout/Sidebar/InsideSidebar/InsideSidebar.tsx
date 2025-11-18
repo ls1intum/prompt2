6c245ed0c56bcfcba0c666b09f31a6dfc1cbe0fc
Josef Schmid <51161233+JGStyle@users.noreply.github.com>
2025-11-18 16:45:35 +0100

    General: Switch to a House Icon for the Home page, add logo to Innersidebar (#927)
    * add icon to InnerSidebar; switch home page to have a house icon
    
    * adapt alt text

--- clients/core/src/managementConsole/layout/Sidebar/InsideSidebar/InsideSidebar.tsx
+++ clients/core/src/managementConsole/layout/Sidebar/InsideSidebar/InsideSidebar.tsx
@@ -14,11 +14,14 @@ export const InsideSidebar = (): JSX.Element => {
   return (
     <Sidebar collapsible='none' className='flex'>
       <SidebarHeader className='flex h-14 border-b justify-center items-center'>
-        <div className='relative flex items-baseline'>
-          <span className='text-lg font-extrabold tracking-wide text-primary drop-shadow-sm'>
-            PROMPT
-          </span>
-          <span className='ml-1 text-xs font-normal text-gray-400'>{version}</span>
+        <div className='flex items-center'>
+          <img src='/prompt_logo.svg' alt='Prompt logo' className='size-8 -mr-1' />
+          <div className='relative flex items-baseline'>
+            <span className='text-lg font-extrabold tracking-wide text-primary drop-shadow-sm'>
+              PROMPT
+            </span>
+            <span className='ml-1 text-xs font-normal text-gray-400'>{version}</span>
+          </div>
         </div>
       </SidebarHeader>
       <SidebarContent>
