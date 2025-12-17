1d907e0ea1062f10060f9eda8902504054b6ca46
Josef Schmid <51161233+JGStyle@users.noreply.github.com>
2025-12-17 17:13:22 +0100

    Core: Course configurator improvements (#959)
    * edit gitinore
    
    * remove 'Required' from input DTOs
    
    * Remove icons from connection arrows
    
    * add description to CoursePhaseType and show it on hover with new help icon
    
    * improve optics of hover descriptoin
    
    * Reorganize layout to make more space for horizontal React Flow
    
    * revert local main.go changes
    
    * simplify Modified Changes Alert
    
    * replace duplicated DefeRollback calls with existing SDK function; and remove the util
    
    * remove duplicate dbTransactionRollback and use promptSDK functionality
    
    * teamallocation server: use intended promptSDK function
    
    * adjust PhaseRouterMapping for Intro Course Developer Renaming
    
    * convert camelCase to TitleCase
    
    * adjust test databases to have description column
    
    * rename DevOpsChallenge to DevOps Challenge; update migration
    
    * make UnsavedChangesAlert work in Darkmode
    
    * CourseConfigurator: Make Arrow Heads of all edges same size, Make Arrow Heads have the same color as the edge itself
    
    * remove idempotent sql migration for existing records
    
    * don't sort coursephasetype directly; regenerate sqlc
    
    ---------
    
    Co-authored-by: Josef Georg Schmid <jgs@MacBook-Pro-von-Josef.local>
    Co-authored-by: Josef Georg Schmid <jgs@w206-5t-v4.eduroam.dynamic.rbg.tum.de>

---
+++ clients/core/src/managementConsole/courseConfigurator/components/CoursePhaseTypePanel.tsx
@@ -0,0 +1,38 @@
+import { useCourseConfigurationState } from '../zustand/useCourseConfigurationStore'
+import { CoursePhaseType } from '../interfaces/coursePhaseType'
+import { CoursePhaseTypePanelItem } from './CoursePhaseTypePanelItem'
+
+interface CoursePhaseTypePanelProps {
+  canEdit: boolean
+}
+
+export const CoursePhaseTypePanel = ({ canEdit }: CoursePhaseTypePanelProps): JSX.Element => {
+  const { coursePhaseTypes, coursePhases } = useCourseConfigurationState()
+
+  const courseHasInitialPhase = coursePhases.some((phase) => phase.isInitialPhase)
+
+  const coursePhaseTypesOrdered = [...coursePhaseTypes].sort((a, b) => {
+    if (a.initialPhase && !b.initialPhase) return -1
+    if (!a.initialPhase && b.initialPhase) return 1
+    return 0
+  })
+
+  const isDraggable = (phase: CoursePhaseType) => {
+    if (!canEdit) return false
+    if (phase.initialPhase && courseHasInitialPhase) return false
+    if (!courseHasInitialPhase && !phase.initialPhase) return false
+    return true
+  }
+
+  return (
+    <div className='p-4 border-b bg-background'>
+      <h2 className='text-lg font-semibold mb-3'>Course Phases</h2>
+
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3'>
+        {coursePhaseTypesOrdered.map((phase) => (
+          <CoursePhaseTypePanelItem key={phase.id} phase={phase} isDraggable={isDraggable(phase)} />
+        ))}
+      </div>
+    </div>
+  )
+}
