4dd357659db434c09c6432eddf1f9435591b1b7f
Maximilian Rapp <maximilian.rapp@tum.de>
2025-12-19 15:40:04 +0100

    feat: add results_released column and remove grading_sheet_visible migration

--- servers/assessment/db/migration/0022_add_results_released.up.sql
+++ servers/assessment/db/migration/0022_add_results_released.up.sql
@@ -1,1 +1,3 @@
 ALTER TABLE course_phase_config ADD COLUMN results_released BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE course_phase_config
+    ADD COLUMN IF NOT EXISTS grading_sheet_visible BOOLEAN NOT NULL DEFAULT FALSE;
--- servers/assessment/db/migration/0023_add_grading_sheet_visibility.up.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE course_phase_config
-    ADD COLUMN IF NOT EXISTS grading_sheet_visible BOOLEAN NOT NULL DEFAULT FALSE;
- 
