d0be65cc0f1ffdfe5b5ce388fd039f4354b6b43f
Josef Schmid <51161233+JGStyle@users.noreply.github.com>
2025-12-22 17:56:54 +0100

    naming: getTimeoutContext

--- servers/core/db/sqlc/getTimeoutContext.go
+++ servers/core/db/sqlc/getTimeoutContext.go
@@ -5,10 +5,8 @@ import (
 	"time"
 )
 
-// DEFAULT_TIMEOUT is used for database calls to avoid hanging requests.
 const DEFAULT_TIMEOUT = 10 * time.Second
 
-// GetTimeoutContext returns a context with a sane default timeout.
 func GetTimeoutContext(ctx context.Context) (context.Context, context.CancelFunc) {
 	return context.WithTimeout(ctx, DEFAULT_TIMEOUT)
 }
