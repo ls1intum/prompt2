62d16aedd23643e802f4f92a385b652ce41e04b8
Josef Schmid <51161233+JGStyle@users.noreply.github.com>
2026-01-14 08:51:33 +0100

    use student avatar in student detail page

--- clients/core/src/managementConsole/pages/StudentDetailPage.tsx
+++ clients/core/src/managementConsole/pages/StudentDetailPage.tsx
@@ -1,10 +1,11 @@
 import { CourseEnrollment } from '@core/network/queries/getStudentEnrollments'
-import { ManagementPageHeader } from '@tumaet/prompt-ui-components'
 import { useParams } from 'react-router-dom'
 import { StudentCourseEnrollment } from '../shared/components/StudentDetail/StudentCourseEnrollment'
 import { useStudent } from '@core/network/hooks/useStudent'
 import { useStudentEnrollments } from '@core/network/hooks/useStudentEnrollments'
 import { Loader2 } from 'lucide-react'
+import { StudentProfile } from '@/components/StudentProfile'
+import { PassStatus } from '@tumaet/prompt-shared-state'
 
 export const StudentDetailPage = () => {
   const { studentId } = useParams<{ studentId: string }>()
@@ -14,10 +15,14 @@ export const StudentDetailPage = () => {
 
   return (
     <div>
-      <ManagementPageHeader>
-        {student.data?.firstName} {student.data?.lastName}
-      </ManagementPageHeader>
-      <div className='flex w-full justify-between gap-4'>
+      <div className='flex flex-col w-full justify-between gap-8'>
+        <div className='flex flex-col gap-y-2 text-sm'>
+          {student.isLoading && <Loader2 className='h-4 w-4 animate-spin' />}
+          {student.isSuccess && (
+            <StudentProfile student={student.data} status={PassStatus.PASSED} />
+          )}
+        </div>
+
         <div className='flex flex-col gap-5'>
           {enrollments.isLoading && <Loader2 className='h-4 w-4 animate-spin' />}
           {enrollments.isSuccess &&
@@ -26,43 +31,6 @@ export const StudentDetailPage = () => {
                 <StudentCourseEnrollment sce={c} />
               </div>
             ))}
-        </div>
-        <div className='flex flex-col gap-y-2 text-sm mr-5'>
-          {student.isLoading && <Loader2 className='h-4 w-4 animate-spin' />}
-          {student.isSuccess && (
-            <>
-              <div>
-                <span className='font-semibold'>Name</span>
-                <div>
-                  {student.data?.firstName} {student.data?.lastName}
-                </div>
-              </div>
-
-              <div>
-                <span className='font-semibold'>ID</span>
-                <div>
-                  {student.data?.matriculationNumber} / {student.data?.universityLogin}
-                </div>
-              </div>
-
-              <div>
-                <span className='font-semibold'>Email</span>
-                <div className='truncate'>{student.data?.email}</div>
-              </div>
-
-              <div>
-                <span className='font-semibold'>Study Program</span>
-                <div>
-                  {student.data?.currentSemester
-                    ? student.data?.currentSemester + '. Semester'
-                    : ''}
-                </div>
-                <div>
-                  {student.data?.studyProgram} ({student.data?.studyDegree})
-                </div>
-              </div>
-            </>
-          )}
         </div>
       </div>
     </div>
