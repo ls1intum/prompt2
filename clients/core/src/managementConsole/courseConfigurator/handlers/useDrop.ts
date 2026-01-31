c0995d3fcc7747ffc65ef7a8e284a664b72fc111
Josef Schmid <51161233+JGStyle@users.noreply.github.com>
2026-01-31 21:42:02 +0100

    remove format string

--- clients/core/src/managementConsole/courseConfigurator/handlers/useDrop.ts
+++ clients/core/src/managementConsole/courseConfigurator/handlers/useDrop.ts
@@ -38,7 +38,7 @@ export const useDrop = (reactFlowWrapper, setNodes, setIsModified) => {
         const coursePhase: CoursePhaseWithPosition = {
           id: id,
           courseID: courseId ?? 'no-valid-id',
-          name: `${coursePhaseType.name}`,
+          name: coursePhaseType.name,
           position: position,
           isInitialPhase: coursePhaseType.initialPhase,
           coursePhaseTypeID: coursePhaseType.id,
