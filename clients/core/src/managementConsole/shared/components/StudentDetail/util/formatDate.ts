bde0588409ee118873e547ae56c269fdd6ecd73e
Josef Schmid <51161233+JGStyle@users.noreply.github.com>
2026-01-31 22:33:48 +0100

    fix formatDate

--- clients/core/src/managementConsole/shared/components/StudentDetail/util/formatDate.ts
+++ clients/core/src/managementConsole/shared/components/StudentDetail/util/formatDate.ts
@@ -1,6 +1,6 @@
 export function formatDate(date: string | null): string {
   if (!date) return '-'
-  return new Date(date).toLocaleDateString('us-US', {
+  return new Date(date).toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'short',
     day: '2-digit',
