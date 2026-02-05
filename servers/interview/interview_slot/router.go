d4179fb44184a5b05d45abfe8b27e40606359eb6
Mathilde Susanna Hagl <mathilde.s.hagl@tum.de>
2026-02-05 11:09:19 +0100

    feat(interview): update authorization roles for interview slot and assignment routes

--- servers/interview/interview_slot/router.go
+++ servers/interview/interview_slot/router.go
@@ -9,16 +9,16 @@ func setupInterviewSlotRouter(routerGroup *gin.RouterGroup, authMiddleware func(
 	interviewRouter := routerGroup.Group("/interview-slots")
 
 	// Admin routes - manage interview slots
-	interviewRouter.POST("", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer), createInterviewSlot)
-	interviewRouter.GET("", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer, promptSDK.PromptLecturer, promptSDK.CourseStudent), getAllInterviewSlots)
-	interviewRouter.GET("/:slotId", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer, promptSDK.PromptLecturer, promptSDK.CourseStudent), getInterviewSlot)
-	interviewRouter.PUT("/:slotId", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer), updateInterviewSlot)
-	interviewRouter.DELETE("/:slotId", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer), deleteInterviewSlot)
+	interviewRouter.POST("", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer, promptSDK.CourseEditor), createInterviewSlot)
+	interviewRouter.GET("", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer, promptSDK.CourseEditor, promptSDK.PromptLecturer, promptSDK.CourseStudent), getAllInterviewSlots)
+	interviewRouter.GET("/:slotId", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer, promptSDK.CourseEditor, promptSDK.PromptLecturer, promptSDK.CourseStudent), getInterviewSlot)
+	interviewRouter.PUT("/:slotId", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer, promptSDK.CourseEditor), updateInterviewSlot)
+	interviewRouter.DELETE("/:slotId", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer, promptSDK.CourseEditor), deleteInterviewSlot)
 
 	// Student routes - book interview slots
 	assignmentRouter := routerGroup.Group("/interview-assignments")
-	assignmentRouter.POST("", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer, promptSDK.PromptLecturer, promptSDK.CourseStudent), createInterviewAssignment)
-	assignmentRouter.POST("/admin", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer), createInterviewAssignmentAdmin)
-	assignmentRouter.GET("/my-assignment", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer, promptSDK.PromptLecturer, promptSDK.CourseStudent), getMyInterviewAssignment)
-	assignmentRouter.DELETE("/:assignmentId", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer, promptSDK.PromptLecturer, promptSDK.CourseStudent), deleteInterviewAssignment)
+	assignmentRouter.POST("", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer, promptSDK.CourseEditor, promptSDK.PromptLecturer, promptSDK.CourseStudent), createInterviewAssignment)
+	assignmentRouter.POST("/admin", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer, promptSDK.CourseEditor), createInterviewAssignmentAdmin)
+	assignmentRouter.GET("/my-assignment", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer, promptSDK.CourseEditor, promptSDK.PromptLecturer, promptSDK.CourseStudent), getMyInterviewAssignment)
+	assignmentRouter.DELETE("/:assignmentId", authMiddleware(promptSDK.PromptAdmin, promptSDK.CourseLecturer, promptSDK.CourseEditor, promptSDK.PromptLecturer, promptSDK.CourseStudent), deleteInterviewAssignment)
 }
